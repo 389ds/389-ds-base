ab37199479f849ecae189e1a2314da49cc906187
Rob Crittenden <rcritten@redhat.com>
2005-03-07 14:49:18 +0000

    149510
    Remove files that aren't needed.

--- include/libaccess/stubs.h
+++
@@ -1,6 +0,0 @@
-/** BEGIN COPYRIGHT BLOCK
- * Copyright (C) 2001 Sun Microsystems, Inc. Used by permission.
- * Copyright (C) 2005 Red Hat, Inc.
- * All rights reserved.
- * END COPYRIGHT BLOCK **/
-typedef	void	PropList_t;
