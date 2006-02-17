1b7b2c95a58f0220abed1928bec962d6f4ee89e9
Rich Megginson <rmeggins@redhat.com>
2006-02-17 15:05:32 +0000

    1) Fix moz objdir name for generic linux x86_64 2) Allow moz objdir name to be passed in

--- buildpaths.mk
+++ buildpaths.mk
@@ -67,17 +67,23 @@
 
 MOZILLA_SOURCE_ROOT = $(BUILD_ROOT)/../mozilla
 ifdef MOZILLA_SOURCE_ROOT
-  # some of the mozilla components are put in a platform/buildtype specific
-  # subdir of mozilla/dist, and their naming convention is different than
-  # ours - we need to map ours to theirs
-  ifneq (,$(findstring RHEL3,$(NSOBJDIR_NAME)))
-    MOZ_OBJDIR_NAME = $(subst _gcc3_,_glibc_PTH$(NS64TAG)_,$(subst RHEL3,Linux2.4,$(NSOBJDIR_NAME)))
-  else
-  ifneq (,$(findstring RHEL4,$(NSOBJDIR_NAME)))
-    MOZ_OBJDIR_NAME = $(subst _gcc3_,_glibc_PTH$(NS64TAG)_,$(subst RHEL4,Linux2.6,$(NSOBJDIR_NAME)))
-  else
-    MOZ_OBJDIR_NAME = $(NSOBJDIR_NAME)
-  endif
+  ifndef MOZ_OBJDIR_NAME
+    # some of the mozilla components are put in a platform/buildtype specific
+    # subdir of mozilla/dist, and their naming convention is different than
+    # ours - we need to map ours to theirs
+    ifneq (,$(findstring RHEL3,$(NSOBJDIR_NAME)))
+      MOZ_OBJDIR_NAME = $(subst _gcc3_,_glibc_PTH$(NS64TAG)_,$(subst RHEL3,Linux2.4,$(NSOBJDIR_NAME)))
+    else
+    ifneq (,$(findstring RHEL4,$(NSOBJDIR_NAME)))
+      MOZ_OBJDIR_NAME = $(subst _gcc3_,_glibc_PTH$(NS64TAG)_,$(subst RHEL4,Linux2.6,$(NSOBJDIR_NAME)))
+    else
+    ifneq (,$(findstring Linux,$(NSOBJDIR_NAME)))
+      MOZ_OBJDIR_NAME = $(subst _glibc_PTH_,_glibc_PTH$(NS64TAG)_,$(NSOBJDIR_NAME))
+    else
+      MOZ_OBJDIR_NAME = $(NSOBJDIR_NAME)
+    endif
+    endif
+    endif
   endif
 endif
 
