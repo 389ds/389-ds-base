da9030b8c3ceb057ddb92d04ceafce3976418438
Nathan Kinder <nkinder@redhat.com>
2006-04-18 18:57:44 +0000

    16578 - Fixed off by one error in month in logconv.pl

--- ldap/admin/src/logconv.pl
+++ ldap/admin/src/logconv.pl
@@ -275,18 +275,18 @@ $connmsg{"P2"} = "Poll";
 $connmsg{"U1"} = "Cleanly Closed Connections";
 
 %monthname = (
-	"Jan" => 1,
-	"Feb" => 2,
-	"Mar" => 3,
-	"Apr" => 4,
-	"May" => 5,
-	"Jun" => 6,
-	"Jul" => 7,
-	"Aug" => 8,
-	"Sep" => 9,
-	"Oct" => 10,
-	"Nov" => 11,
-	"Dec" => 12,
+	"Jan" => 0,
+	"Feb" => 1,
+	"Mar" => 2,
+	"Apr" => 3,
+	"May" => 4,
+	"Jun" => 5,
+	"Jul" => 6,
+	"Aug" => 7,
+	"Sep" => 8,
+	"Oct" => 9,
+	"Nov" => 10,
+	"Dec" => 11,
 
 );
 
