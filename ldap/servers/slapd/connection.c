5b23a738b302e27b3d0d2d9b7c15381c0a30c4c0
Noriko Hosoi <nhosoi@redhat.com>
2007-05-14 22:04:55 +0000

    Resolves: #237731 Summary: Random SASL GSSAPI test failure on shadowfoot (Comment #9)

--- ldap/servers/slapd/connection.c
+++ ldap/servers/slapd/connection.c
@@ -1745,8 +1745,9 @@ int connection_read_operation(Connection *conn, Operation *op, ber_tag_t *tag, i
 			}
 			/* err = PR_GetError(); */
 			/* If we would block, we need to poll for a while */
+			syserr = PR_GetOSError();
 			if ( SLAPD_PR_WOULD_BLOCK_ERROR( err ) ||
-			     SLAPD_SYSTEM_WOULD_BLOCK_ERROR( err ) ) {
+			     SLAPD_SYSTEM_WOULD_BLOCK_ERROR( syserr ) ) {
 				struct PRPollDesc	pr_pd;
 				PRIntervalTime	timeout = PR_MillisecondsToInterval(CONN_TURBO_TIMEOUT_INTERVAL);
 				pr_pd.fd = (PRFileDesc *)conn->c_prfd;
