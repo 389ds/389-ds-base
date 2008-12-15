9d681c65647ad8d895ae3a3f818eb7ca006b241e
Nathan Kinder <nkinder@redhat.com>
2008-12-15 17:42:25 +0000

    Resolves: 430368 Summary: Add check for permissions on nsslapd-rundir at startup.

--- ldap/servers/slapd/main.c
+++ ldap/servers/slapd/main.c
@@ -978,6 +978,17 @@ main( int argc, char **argv)
 		exit(1);
 	}
 
+	/* Ensure that we can read from and write to our rundir */
+	if (access(config_get_rundir(), R_OK | W_OK)) {
+		LDAPDebug(LDAP_DEBUG_ANY, "Unable to access nsslapd-rundir: %s\n",
+			slapd_system_strerror(errno), 0, 0);
+		LDAPDebug(LDAP_DEBUG_ANY, "Ensure that user \"%s\" has read and write "
+			"permissions on %s\n",
+			slapdFrontendConfig->localuser, config_get_rundir(), 0);
+		LDAPDebug(LDAP_DEBUG_ANY, "Shutting down.\n", 0, 0, 0);
+		exit(1);
+	}
+
 	/*
 	 * Detach ourselves from the terminal (unless running in debug mode).
 	 * We must detach before we start any threads since detach forks() on
