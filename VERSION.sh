eb0065aef9dbb05cbcc725684680d16e019f9232
Rich Megginson <rmeggins@redhat.com>
2010-04-15 12:06:11 -0600

    start of 1.2.6.a4

--- VERSION.sh
+++ VERSION.sh
@@ -14,7 +14,7 @@ VERSION_MAINT=6
 # if this is a PRERELEASE, set VERSION_PREREL
 # otherwise, comment it out
 # be sure to include the dot prefix in the prerel
-VERSION_PREREL=.a3
+VERSION_PREREL=.a4
 # NOTES on VERSION_PREREL
 # use aN for an alpha release e.g. a1, a2, etc.
 # use rcN for a release candidate e.g. rc1, rc2, etc.
