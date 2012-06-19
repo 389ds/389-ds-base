edcd811a547cb9ec9bb68af54a6373377a61c65c
Rich Megginson <rmeggins@redhat.com>
2012-06-18 18:20:58 -0600

    bump version to 1.2.10.10

--- VERSION.sh
+++ VERSION.sh
@@ -10,7 +10,7 @@ vendor="389 Project"
 # PACKAGE_VERSION is constructed from these
 VERSION_MAJOR=1
 VERSION_MINOR=2
-VERSION_MAINT=10.8
+VERSION_MAINT=10.10
 # if this is a PRERELEASE, set VERSION_PREREL
 # otherwise, comment it out
 # be sure to include the dot prefix in the prerel
