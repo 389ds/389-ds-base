389ds/389-ds-base

commit 7f81635990fa340e2db5c1b14a8d1ba10fa53887

Trac Ticket #391 - Slapd crashes when deleting backends     while operations are still in progress
https://fedorahosted.org/389/ticket/391

Bug Description: Deleting backend code ldbm_instance_delete_instance_
entry_callback had no checking for the ordinary operations accessing
the backend instance.  Even if some operations are still in progress,
the backend instance could be deleted and it crashes the server.

Fix Description: Backend struct ldbm_instance had a member inst_ref_
count, which was not used.  This patch converts the type PRInt32 to
Slapi_Counter and increments it when the backend instance is in use.
The delete code checks the counter and if it is greater than 0, it
returns SLAPI_DSE_CALLBACK_ERROR.

ldap/servers/slapd/back-ldbm/ldbm_add.c
@@ -132,7 +132,15 @@ ldbm_back_add( Slapi_PBlock *pb )
 	is_ruv = operation_is_flag_set(operation, OP_FLAG_REPL_RUV);
 
 	inst = (ldbm_instance *) be->be_instance_info;
-		
+	if (inst->inst_ref_count) {
+		slapi_counter_increment(inst->inst_ref_count);
+	} else {
+		LDAPDebug1Arg(LDAP_DEBUG_ANY,
+		              "ldbm_add: instance %s does not exist.\n",
+		              inst->inst_name);
+		goto error_return;
+	}
+
 	/* sdn & parentsdn need to be initialized before "goto *_return" */
 	slapi_sdn_init(&parentsdn);
 	
@@ -878,8 +886,9 @@ ldbm_back_add( Slapi_PBlock *pb )
 			continue;
 		}
 		if (retval != 0) {
-			LDAPDebug( LDAP_DEBUG_ANY, "add: attempt to index %lu failed\n",
-								   (u_long)addingentry->ep_id, 0, 0 );
+			LDAPDebug2Args(LDAP_DEBUG_ANY,
+			               "add: attempt to index %lu failed (rc=%d)\n",
+			               (u_long)addingentry->ep_id, retval);
 			ADD_SET_ERROR(ldap_result_code, LDAP_OPERATIONS_ERROR, retry_count);
 			if (LDBM_OS_ERR_IS_DISKFULL(retval)) {
 				disk_full = 1;
@@ -1137,6 +1146,9 @@ common_return:
         }
         CACHE_RETURN( &inst->inst_cache, &addingentry );
     }
+	if (inst->inst_ref_count) {
+		slapi_counter_decrement(inst->inst_ref_count);
+	}
 	/* bepost op needs to know this result */
 	slapi_pblock_set(pb, SLAPI_RESULT_CODE, &ldap_result_code);
 	/* JCMREPL - The bepostop is called even if the operation fails. */

ldap/servers/slapd/back-ldbm/ldbm_compare.c
@@ -61,7 +61,6 @@ ldbm_back_compare( Slapi_PBlock *pb )
 	Slapi_DN *namespace_dn;
 	back_txn txn = {NULL};
 
-
 	slapi_pblock_get( pb, SLAPI_BACKEND, &be );
 	slapi_pblock_get( pb, SLAPI_PLUGIN_PRIVATE, &li );
 	slapi_pblock_get( pb, SLAPI_TARGET_ADDRESS, &addr);
@@ -75,16 +74,25 @@ ldbm_back_compare( Slapi_PBlock *pb )
 	}
 
 	inst = (ldbm_instance *) be->be_instance_info;
+	if (inst->inst_ref_count) {
+		slapi_counter_increment(inst->inst_ref_count);
+	} else {
+		LDAPDebug1Arg(LDAP_DEBUG_ANY,
+		              "ldbm_compare: instance %s does not exist.\n",
+		              inst->inst_name);
+		return -1;
+	}
 	/* get the namespace dn */
 	namespace_dn = (Slapi_DN*)slapi_be_getsuffix(be, 0);
 
 	if ( (e = find_entry( pb, be, addr, &txn )) == NULL ) {
-		return( -1 );	/* error result sent by find_entry() */
+		ret = -1;	/* error result sent by find_entry() */
+		goto bail;
 	}
 
 	err = slapi_access_allowed (pb, e->ep_entry, type, bval, SLAPI_ACL_COMPARE);
 	if ( err != LDAP_SUCCESS ) {
-		slapi_send_ldap_result( pb, err, NULL, NULL, 0, NULL );								
+		slapi_send_ldap_result( pb, err, NULL, NULL, 0, NULL );
 		ret = 1;
 	} else {
 
@@ -117,5 +125,9 @@ ldbm_back_compare( Slapi_PBlock *pb )
 	}
 
 	CACHE_RETURN( &inst->inst_cache, &e );
+bail:
+	if (inst->inst_ref_count) {
+		slapi_counter_decrement(inst->inst_ref_count);
+	}
 	return( ret );
 }

ldap/servers/slapd/back-ldbm/ldbm_delete.c
@@ -146,6 +146,14 @@ ldbm_back_delete( Slapi_PBlock *pb )
 	delete_tombstone_entry = operation_is_flag_set(operation, OP_FLAG_TOMBSTONE_ENTRY);
 	
 	inst = (ldbm_instance *) be->be_instance_info;
+	if (inst->inst_ref_count) {
+		slapi_counter_increment(inst->inst_ref_count);
+	} else {
+		LDAPDebug1Arg(LDAP_DEBUG_ANY,
+		              "ldbm_delete: instance %s does not exist.\n",
+		              inst->inst_name);
+		goto error_return;
+	}
 
 	/* The dblock serializes writes to the database,
 	 * which reduces deadlocking in the db code,
@@ -1206,6 +1214,9 @@ common_return:
 			CACHE_RETURN( &inst->inst_cache, &e );
 		}
 	}
+	if (inst->inst_ref_count) {
+		slapi_counter_decrement(inst->inst_ref_count);
+	}
 	
 	if (ruv_c_init) {
 		modify_term(&ruv_c, be);
@@ -1214,7 +1225,7 @@ common_return:
 diskfull_return:
     if(ldap_result_code!=-1)
 	{
-    	slapi_send_ldap_result( pb, ldap_result_code, NULL, ldap_result_message, 0, NULL );
+		slapi_send_ldap_result( pb, ldap_result_code, NULL, ldap_result_message, 0, NULL );
 	}
 	modify_term(&parent_modify_c,be);
 	if (rc == 0 && opcsn && !is_fixup_operation && !delete_tombstone_entry)

ldap/servers/slapd/back-ldbm/ldbm_modify.c
@@ -368,6 +368,14 @@ ldbm_back_modify( Slapi_PBlock *pb )
 	{
 		goto error_return;
 	}
+	if (inst->inst_ref_count) {
+		slapi_counter_increment(inst->inst_ref_count);
+	} else {
+		LDAPDebug1Arg(LDAP_DEBUG_ANY,
+		              "ldbm_modify: instance %s does not exist.\n",
+		              inst->inst_name);
+		goto error_return;
+	}
 
 	/* no need to check the dn syntax as this is a replicated op */
 	if(!repl_op){
@@ -812,6 +820,9 @@ common_return:
 	if (e!=NULL) {
 		cache_unlock_entry( &inst->inst_cache, e);
 		CACHE_RETURN( &inst->inst_cache, &e);
+	}
+	if (inst->inst_ref_count) {
+		slapi_counter_decrement(inst->inst_ref_count);
 	}
 
 	/* result code could be used in the bepost plugin functions. */

ldap/servers/slapd/back-ldbm/ldbm_modrdn.c
@@ -191,6 +191,15 @@ ldbm_back_modrdn( Slapi_PBlock *pb )
         return( -1 );
     } 
 
+    if (inst->inst_ref_count) {
+        slapi_counter_increment(inst->inst_ref_count);
+    } else {
+        LDAPDebug1Arg(LDAP_DEBUG_ANY,
+                      "ldbm_modrdn: instance %s does not exist.\n",
+                      inst->inst_name);
+        return( -1 );
+    }
+
     /* The dblock serializes writes to the database,
      * which reduces deadlocking in the db code,
      * which means that we run faster.
@@ -1291,6 +1300,9 @@ common_return:
             CACHE_ADD(&inst->inst_cache, e, NULL);
             e_in_cache = 1;
         }
+    }
+    if (inst->inst_ref_count) {
+        slapi_counter_decrement(inst->inst_ref_count);
     }
 
     moddn_unlock_and_return_entry(be,&e);

ldap/servers/slapd/back-ldbm/ldbm_search.c
@@ -169,6 +169,9 @@ ldbm_back_search_cleanup(Slapi_PBlock *pb,
     slapi_pblock_get( pb, SLAPI_BACKEND, &be );
     inst = (ldbm_instance *) be->be_instance_info;
     CACHE_RETURN(&inst->inst_cache, &e); /* NULL e is handled correctly */
+    if (inst->inst_ref_count) {
+        slapi_counter_decrement(inst->inst_ref_count);
+    }
 
     if(sort_control!=NULL)
     {
@@ -343,6 +346,7 @@ ldbm_back_search( Slapi_PBlock *pb )
     int backend_count = 1;
     static int print_once = 1;
     back_txn txn = {NULL};
+    int rc = 0;
 
     slapi_pblock_get( pb, SLAPI_BACKEND, &be );
     slapi_pblock_get( pb, SLAPI_OPERATION, &operation);
@@ -366,6 +370,14 @@ ldbm_back_search( Slapi_PBlock *pb )
                                "Null target DN", 0, NULL );
         return( -1 );
     }
+    if (inst->inst_ref_count) {
+        slapi_counter_increment(inst->inst_ref_count);
+    } else {
+        LDAPDebug1Arg(LDAP_DEBUG_ANY,
+                      "ldbm_search: instance %s does not exist.\n",
+                      inst->inst_name);
+        return( -1 );
+    }
     base = slapi_sdn_get_dn(basesdn);
 
     /* Initialize the result set structure here because we need to use it during search processing */
@@ -383,9 +395,8 @@ ldbm_back_search( Slapi_PBlock *pb )
         sort = slapi_control_present( controls, LDAP_CONTROL_SORTREQUEST, &sort_spec, &is_sorting_critical_orig );
         if(sort)
         {
-            int r= parse_sort_spec(sort_spec, &sort_control);
-            if(r!=0)
-            {
+            rc = parse_sort_spec(sort_spec, &sort_control);
+            if (rc) {
                 /* Badly formed SORT control */
                 return ldbm_back_search_cleanup(pb, li, sort_control, 
                                 LDAP_PROTOCOL_ERROR, "Sort Control", 
@@ -401,12 +412,11 @@ ldbm_back_search( Slapi_PBlock *pb )
         {
             if(sort)
             {
-                int r = vlv_parse_request_control( be, vlv_spec, &vlv_request_control );
-                if(r!=LDAP_SUCCESS)
-                {
+                rc = vlv_parse_request_control( be, vlv_spec, &vlv_request_control );
+                if (rc != LDAP_SUCCESS) {
                     /* Badly formed VLV control */
                     return ldbm_back_search_cleanup(pb, li, sort_control,
-                                r, "VLV Control", SLAPI_FAIL_GENERAL, 
+                                rc, "VLV Control", SLAPI_FAIL_GENERAL, 
                                 &vlv_request_control, NULL);
                 }
                 {
@@ -421,13 +431,12 @@ ldbm_back_search( Slapi_PBlock *pb )
                     /* This dn is normalized. */
                     PR_snprintf(dn,sizeof(dn),"dn: oid=%s,cn=features,cn=config",LDAP_CONTROL_VLVREQUEST);
                     feature= slapi_str2entry(dn,0);
-                    r= plugin_call_acl_plugin (pb, feature, dummyAttrs, NULL, SLAPI_ACL_READ, ACLPLUGIN_ACCESS_DEFAULT, NULL);
+                    rc = plugin_call_acl_plugin (pb, feature, dummyAttrs, NULL, SLAPI_ACL_READ, ACLPLUGIN_ACCESS_DEFAULT, NULL);
                     slapi_entry_free(feature);
-                    if(r!=LDAP_SUCCESS)
-                    {
+                    if (rc != LDAP_SUCCESS) {
                         /* Client isn't allowed to do this. */
                         return ldbm_back_search_cleanup(pb, li, sort_control, 
-                                    r, "VLV Control", SLAPI_FAIL_GENERAL, 
+                                    rc, "VLV Control", SLAPI_FAIL_GENERAL, 
                                     &vlv_request_control, NULL);
                     }
                 }
@@ -650,22 +659,20 @@ ldbm_back_search( Slapi_PBlock *pb )
              */
             if (virtual_list_view && (NULL != candidates))
             {
-                int r = 0;
                 IDList *idl = NULL;
                 Slapi_Filter *filter = NULL;
                 slapi_pblock_get( pb, SLAPI_SEARCH_FILTER, &filter );
-                r = vlv_filter_candidates(be, pb, candidates, basesdn,
+                rc = vlv_filter_candidates(be, pb, candidates, basesdn,
                                           scope, filter, &idl,
                                           lookthrough_limit, time_up);
-                if(r == 0)
-                {
+                if (rc == 0) {
                     idl_free(candidates);
                     candidates= idl;
                 }
                 else
                 {
                     return ldbm_back_search_cleanup(pb, li, sort_control,
-                                                    r, NULL, -1, 
+                                                    rc, NULL, -1, 
                                                     &vlv_request_control, e);
                 }
             }

ldap/servers/slapd/back-ldbm/back-ldbm.h
@@ -735,10 +735,8 @@ typedef struct ldbm_instance {
     int inst_flags;                   /* see above */
 
     PRLock *inst_config_mutex;
-
-    PRInt32 *inst_ref_count;          /* Keeps track of how many operations 
+    Slapi_Counter *inst_ref_count;    /* Keeps track of how many operations 
                                        * are currently using this instance */
-
     char *inst_dir_name;              /* The name of the directory in the db
                                        * directory that holds the index files
                                        * for this instance. Relative to the

ldap/servers/slapd/back-ldbm/dblayer.c
@@ -3103,14 +3103,20 @@ out:
     slapi_ch_free((void**)&rel_path);
     /* close the database handle to avoid handle leak */
     if (dbp && (return_value != 0)) {
-        dblayer_close_file(dbp);
+        dblayer_close_file(&dbp);
     }
     return return_value;
 }
 
-int dblayer_close_file(DB *db)
+int 
+dblayer_close_file(DB **db)
 {
-    return db->close(db,0);
+    if (db) {
+        DB *dbp = *db;
+        *db = NULL; /* To avoid to leave stale DB, set NULL before closing. */
+        return dbp->close(dbp, 0);
+    }
+    return 1;
 }
 
 /*
@@ -3163,7 +3169,7 @@ int dblayer_get_index_file(backend *be, struct attrinfo *a, DB** ppDB, int open_
    */
   PR_AtomicIncrement(&a->ai_dblayer_count);
   
-  if (NULL != a->ai_dblayer) {
+  if (a->ai_dblayer && ((dblayer_handle*)(a->ai_dblayer))->dblayer_dbp) {
     /* This means that the pointer is valid, so we should return it. */
     *ppDB = ((dblayer_handle*)(a->ai_dblayer))->dblayer_dbp;
     return 0;
@@ -3173,7 +3179,7 @@ int dblayer_get_index_file(backend *be, struct attrinfo *a, DB** ppDB, int open_
    * again.
    */
   PR_Lock(inst->inst_handle_list_mutex);
-  if (NULL != a->ai_dblayer) {
+  if (a->ai_dblayer && ((dblayer_handle*)(a->ai_dblayer))->dblayer_dbp) {
     /* another thread set the handle while we were waiting on the lock */
     *ppDB = ((dblayer_handle*)(a->ai_dblayer))->dblayer_dbp;
     PR_Unlock(inst->inst_handle_list_mutex);
@@ -3316,6 +3322,7 @@ int dblayer_erase_index_file_ex(backend *be, struct attrinfo *a,
 
   if (0 == dblayer_get_index_file(be, a, &db, 0 /* Don't create an index file
                                                    if it does not exist. */)) {
+    if(use_lock) slapi_rwlock_wrlock(pEnv->dblayer_env_lock); /* We will be causing logging activity */
     /* first, remove the file handle for this index, if we have it open */
     PR_Lock(inst->inst_handle_list_mutex);
     if (a->ai_dblayer) {
@@ -3340,7 +3347,7 @@ int dblayer_erase_index_file_ex(backend *be, struct attrinfo *a,
         DS_Sleep(DBLAYER_CACHE_DELAY);
         PR_Lock(inst->inst_handle_list_mutex);
       }
-      dblayer_close_file(handle->dblayer_dbp);
+      dblayer_close_file(&(handle->dblayer_dbp));
       
       /* remove handle from handle-list */
       if (inst->inst_handle_head == handle) {
@@ -3372,7 +3379,7 @@ int dblayer_erase_index_file_ex(backend *be, struct attrinfo *a,
         p = dbNamep + dbbasenamelen;
         sprintf(p, "%c%s%s",
                    get_sep(dbNamep), a->ai_type, LDBM_FILENAME_SUFFIX);
-        rc = dblayer_db_remove_ex(pEnv, dbNamep, 0, use_lock);
+        rc = dblayer_db_remove_ex(pEnv, dbNamep, 0, 0);
         a->ai_dblayer = NULL;
         if (dbNamep != dbName)
           slapi_ch_free_string(&dbNamep);
@@ -3386,6 +3393,7 @@ int dblayer_erase_index_file_ex(backend *be, struct attrinfo *a,
       /* no handle to close */
     }
     PR_Unlock(inst->inst_handle_list_mutex);
+    if(use_lock) slapi_rwlock_unlock(pEnv->dblayer_env_lock);
 
   }
   

commit e229bb45b418371d83af302f3d24f881ca32bd41

Coverity fixes
13107,13108: Explicit null deferenced

Bug description: "Explicit null dereferenced" error was introduced
by commit 7f81635990fa340e2db5c1b14a8d1ba10fa53887
Trac Ticket #391 - Slapd crashes when deleting backends
while operations are still in progress

Fix description: Added codes to check if the inst variable is NULL
or not.  If NULL, skip accessing the instance variable and return
an error.

ldap/servers/slapd/back-ldbm/ldbm_add.c
@@ -78,7 +78,7 @@ ldbm_back_add( Slapi_PBlock *pb )
 {
 	backend *be;
 	struct ldbminfo *li;
-	ldbm_instance *inst;
+	ldbm_instance *inst = NULL;
 	const char *dn = NULL;
 	Slapi_Entry	*e = NULL;
 	struct backentry *tombstoneentry = NULL;
@@ -102,7 +102,7 @@ ldbm_back_add( Slapi_PBlock *pb )
 	modify_context ruv_c = {0};
 	int parent_found = 0;
 	int ruv_c_init = 0;
-	int rc;
+	int rc = 0;
 	int addingentry_id_assigned= 0;
 	int addingentry_in_cache= 0;
 	int tombstone_in_cache= 0;
@@ -132,12 +132,12 @@ ldbm_back_add( Slapi_PBlock *pb )
 	is_ruv = operation_is_flag_set(operation, OP_FLAG_REPL_RUV);
 
 	inst = (ldbm_instance *) be->be_instance_info;
-	if (inst->inst_ref_count) {
+	if (inst && inst->inst_ref_count) {
 		slapi_counter_increment(inst->inst_ref_count);
 	} else {
 		LDAPDebug1Arg(LDAP_DEBUG_ANY,
-		              "ldbm_add: instance %s does not exist.\n",
-		              inst->inst_name);
+		              "ldbm_add: instance \"%s\" does not exist.\n",
+		              inst ? inst->inst_name : "null instance");
 		goto error_return;
 	}
 
@@ -1060,13 +1060,15 @@ error_return:
 	{
 		if ( addingentry_in_cache )
 		{
-			CACHE_REMOVE(&inst->inst_cache, addingentry);
+			if (inst) {
+				CACHE_REMOVE(&inst->inst_cache, addingentry);
+			}
 			addingentry_in_cache = 0;
 		}
 		backentry_clear_entry(addingentry); /* e is released in the frontend */
 		backentry_free( &addingentry ); /* release the backend wrapper, here */
 	}
-	if(tombstone_in_cache)
+	if(tombstone_in_cache && inst)
 	{
 		CACHE_RETURN(&inst->inst_cache, &tombstoneentry);
 	}
@@ -1075,8 +1077,9 @@ error_return:
 		dblayer_remember_disk_filled(li);
 		ldbm_nasty("Add",80,rc);
 		disk_full = 1;
+	} else if (0 == rc) {
+		rc = SLAPI_FAIL_GENERAL;
 	}
-
 diskfull_return:
 	if (disk_full) {
 		rc= return_on_disk_full(li);
@@ -1121,33 +1124,34 @@ diskfull_return:
 	}
 	
 common_return:
-    if (addingentry_in_cache && addingentry)
-	{
-        if (entryrdn_get_switch()) { /* subtree-rename: on */
-            /* since adding the entry to the entry cache was successful,
-             * let's add the dn to dncache, if not yet done. */
-            struct backdn *bdn = dncache_find_id(&inst->inst_dncache,
-                                                 addingentry->ep_id);
-            if (bdn) { /* already in the dncache */
-                CACHE_RETURN(&inst->inst_dncache, &bdn);
-            } else { /* not in the dncache yet */
-                Slapi_DN *addingsdn = 
-                      slapi_sdn_dup(slapi_entry_get_sdn(addingentry->ep_entry));
-                if (addingsdn) {
-                    bdn = backdn_init(addingsdn, addingentry->ep_id, 0);
-                    if (bdn) {
-                        CACHE_ADD( &inst->inst_dncache, bdn, NULL );
-                        CACHE_RETURN(&inst->inst_dncache, &bdn);
-                        slapi_log_error(SLAPI_LOG_CACHE, "ldbm_back_add",
-                                                    "set %s to dn cache\n", dn);
-                    }
-                }
-            }
-        }
-        CACHE_RETURN( &inst->inst_cache, &addingentry );
-    }
-	if (inst->inst_ref_count) {
-		slapi_counter_decrement(inst->inst_ref_count);
+	if (inst) {
+		if (addingentry_in_cache && addingentry) {
+			if (entryrdn_get_switch()) { /* subtree-rename: on */
+				/* since adding the entry to the entry cache was successful,
+				 * let's add the dn to dncache, if not yet done. */
+				struct backdn *bdn = dncache_find_id(&inst->inst_dncache,
+				                                     addingentry->ep_id);
+				if (bdn) { /* already in the dncache */
+					CACHE_RETURN(&inst->inst_dncache, &bdn);
+				} else { /* not in the dncache yet */
+					Slapi_DN *addingsdn = 
+					  slapi_sdn_dup(slapi_entry_get_sdn(addingentry->ep_entry));
+					if (addingsdn) {
+						bdn = backdn_init(addingsdn, addingentry->ep_id, 0);
+						if (bdn) {
+							CACHE_ADD( &inst->inst_dncache, bdn, NULL );
+							CACHE_RETURN(&inst->inst_dncache, &bdn);
+							slapi_log_error(SLAPI_LOG_CACHE, "ldbm_back_add",
+							                "set %s to dn cache\n", dn);
+						}
+					}
+				}
+			}
+			CACHE_RETURN( &inst->inst_cache, &addingentry );
+		}
+		if (inst->inst_ref_count) {
+			slapi_counter_decrement(inst->inst_ref_count);
+		}
 	}
 	/* bepost op needs to know this result */
 	slapi_pblock_set(pb, SLAPI_RESULT_CODE, &ldap_result_code);

ldap/servers/slapd/back-ldbm/ldbm_compare.c
@@ -74,12 +74,12 @@ ldbm_back_compare( Slapi_PBlock *pb )
 	}
 
 	inst = (ldbm_instance *) be->be_instance_info;
-	if (inst->inst_ref_count) {
+	if (inst && inst->inst_ref_count) {
 		slapi_counter_increment(inst->inst_ref_count);
 	} else {
 		LDAPDebug1Arg(LDAP_DEBUG_ANY,
-		              "ldbm_compare: instance %s does not exist.\n",
-		              inst->inst_name);
+		              "ldbm_compare: instance \"%s\" does not exist.\n",
+		              inst ? inst->inst_name : "null instance");
 		return -1;
 	}
 	/* get the namespace dn */

ldap/servers/slapd/back-ldbm/ldbm_delete.c
@@ -146,12 +146,12 @@ ldbm_back_delete( Slapi_PBlock *pb )
 	delete_tombstone_entry = operation_is_flag_set(operation, OP_FLAG_TOMBSTONE_ENTRY);
 	
 	inst = (ldbm_instance *) be->be_instance_info;
-	if (inst->inst_ref_count) {
+	if (inst && inst->inst_ref_count) {
 		slapi_counter_increment(inst->inst_ref_count);
 	} else {
 		LDAPDebug1Arg(LDAP_DEBUG_ANY,
-		              "ldbm_delete: instance %s does not exist.\n",
-		              inst->inst_name);
+		              "ldbm_delete: instance \"%s\" does not exist.\n",
+		              inst ? inst->inst_name : "null instance");
 		goto error_return;
 	}
 
@@ -1116,7 +1116,7 @@ ldbm_back_delete( Slapi_PBlock *pb )
 	goto common_return;
 
 error_return:
-	if (tombstone_in_cache)
+	if (inst && tombstone_in_cache)
 	{
 		CACHE_REMOVE( &inst->inst_cache, tombstone );
 		CACHE_RETURN( &inst->inst_cache, &tombstone );
@@ -1186,7 +1186,7 @@ common_return:
 		   for the post op plugins */
 		slapi_pblock_set( pb, SLAPI_DELETE_BEPREOP_ENTRY, orig_entry );
 	}
-	if (tombstone_in_cache)
+	if (inst && tombstone_in_cache)
 	{
 		CACHE_RETURN( &inst->inst_cache, &tombstone );
 		tombstone = NULL;
@@ -1208,14 +1208,14 @@ common_return:
 	}
 
 	/* Need to return to cache after post op plugins are called */
-	if (retval) { /* error case */
-		if (e) {
+	if (inst) {
+		if (retval && e) { /* error case */
 			cache_unlock_entry( &inst->inst_cache, e );
 			CACHE_RETURN( &inst->inst_cache, &e );
 		}
-	}
-	if (inst->inst_ref_count) {
-		slapi_counter_decrement(inst->inst_ref_count);
+		if (inst->inst_ref_count) {
+			slapi_counter_decrement(inst->inst_ref_count);
+		}
 	}
 	
 	if (ruv_c_init) {

ldap/servers/slapd/back-ldbm/ldbm_modify.c
@@ -368,12 +368,12 @@ ldbm_back_modify( Slapi_PBlock *pb )
 	{
 		goto error_return;
 	}
-	if (inst->inst_ref_count) {
+	if (inst && inst->inst_ref_count) {
 		slapi_counter_increment(inst->inst_ref_count);
 	} else {
 		LDAPDebug1Arg(LDAP_DEBUG_ANY,
-		              "ldbm_modify: instance %s does not exist.\n",
-		              inst->inst_name);
+		              "ldbm_modify: instance \"%s\" does not exist.\n",
+		              inst ? inst->inst_name : "null instance");
 		goto error_return;
 	}
 
@@ -795,7 +795,7 @@ error_return:
 	}
 
 	/* if ec is in cache, remove it, then add back e if we still have it */
-	if (ec_in_cache) {
+	if (inst && ec_in_cache) {
 		CACHE_REMOVE( &inst->inst_cache, ec );
 		/* if ec was in cache, e was not - add back e */
 		if (e) {
@@ -807,7 +807,7 @@ error_return:
 common_return:
 	slapi_mods_done(&smods);
 	
-	if (ec_in_cache)
+	if (inst && ec_in_cache)
 	{
 		cache_unlock_entry( &inst->inst_cache, ec);
 		CACHE_RETURN( &inst->inst_cache, &ec );
@@ -817,12 +817,14 @@ common_return:
 		backentry_free(&ec);
 	}
 	
-	if (e!=NULL) {
-		cache_unlock_entry( &inst->inst_cache, e);
-		CACHE_RETURN( &inst->inst_cache, &e);
-	}
-	if (inst->inst_ref_count) {
-		slapi_counter_decrement(inst->inst_ref_count);
+	if (inst) {
+		if (e) {
+			cache_unlock_entry( &inst->inst_cache, e);
+			CACHE_RETURN( &inst->inst_cache, &e);
+		}
+		if (inst->inst_ref_count) {
+			slapi_counter_decrement(inst->inst_ref_count);
+		}
 	}
 
 	/* result code could be used in the bepost plugin functions. */

ldap/servers/slapd/back-ldbm/ldbm_modrdn.c
@@ -63,7 +63,7 @@ int
 ldbm_back_modrdn( Slapi_PBlock *pb )
 {
     backend *be;
-    ldbm_instance *inst;
+    ldbm_instance *inst = NULL;
     struct ldbminfo  *li;
     struct backentry *e= NULL;
     struct backentry *ec= NULL;
@@ -191,12 +191,12 @@ ldbm_back_modrdn( Slapi_PBlock *pb )
         return( -1 );
     } 
 
-    if (inst->inst_ref_count) {
+    if (inst && inst->inst_ref_count) {
         slapi_counter_increment(inst->inst_ref_count);
     } else {
         LDAPDebug1Arg(LDAP_DEBUG_ANY,
-                      "ldbm_modrdn: instance %s does not exist.\n",
-                      inst->inst_name);
+                      "ldbm_modrdn: instance \"%s\" does not exist.\n",
+                      inst ? inst->inst_name : "null instance");
         return( -1 );
     }
 
@@ -1177,7 +1177,7 @@ error_return:
         /* make sure caller doesn't attempt to free this */
         slapi_pblock_set( pb, SLAPI_ENTRY_POST_OP, postentry );
     }
-    if (e && entryrdn_get_switch())
+    if (e && entryrdn_get_switch() && inst)
     {
         struct backdn *bdn = dncache_find_id(&inst->inst_dncache, e->ep_id);
         CACHE_REMOVE(&inst->inst_dncache, bdn);
@@ -1186,7 +1186,7 @@ error_return:
     if(children)
     {
         int i = 0;
-        if (child_entries && *child_entries)
+        if (child_entries && *child_entries && inst)
         {
             if (entryrdn_get_switch()) /* subtree-rename: on */
             {
@@ -1208,7 +1208,7 @@ error_return:
                 }
             }
         }
-        if (entryrdn_get_switch() && child_dns && *child_dns)
+        if (entryrdn_get_switch() && child_dns && *child_dns && inst)
         {
             for (i = 0; child_dns[i] != NULL; i++) {
                 CACHE_REMOVE( &inst->inst_dncache, child_dns[i] );
@@ -1282,10 +1282,10 @@ common_return:
     if (ec) {
         /* remove the new entry from the cache if the op failed -
            otherwise, leave it in */
-        if (ec_in_cache && retval) {
-            CACHE_REMOVE( &inst->inst_cache, ec );
-        }
-        if (ec_in_cache) {
+        if (ec_in_cache && inst) {
+            if (retval) {
+                CACHE_REMOVE( &inst->inst_cache, ec );
+            }
             CACHE_RETURN( &inst->inst_cache, &ec );
         } else {
             backentry_free( &ec );
@@ -1296,12 +1296,12 @@ common_return:
 
     /* put e back in the cache if the modrdn failed */
     if (e) {
-        if (!e_in_cache && retval) {
+        if (!e_in_cache && retval && inst) {
             CACHE_ADD(&inst->inst_cache, e, NULL);
             e_in_cache = 1;
         }
     }
-    if (inst->inst_ref_count) {
+    if (inst && inst->inst_ref_count) {
         slapi_counter_decrement(inst->inst_ref_count);
     }
 

ldap/servers/slapd/back-ldbm/ldbm_search.c
@@ -363,19 +363,18 @@ ldbm_back_search( Slapi_PBlock *pb )
         slapi_pblock_set( pb, SLAPI_TXN, txn.back_txn_txn );
     }
 
-    inst = (ldbm_instance *) be->be_instance_info;
-
     if (NULL == basesdn) {
         slapi_send_ldap_result( pb, LDAP_INVALID_DN_SYNTAX, NULL,
                                "Null target DN", 0, NULL );
         return( -1 );
     }
-    if (inst->inst_ref_count) {
+    inst = (ldbm_instance *) be->be_instance_info;
+    if (inst && inst->inst_ref_count) {
         slapi_counter_increment(inst->inst_ref_count);
     } else {
         LDAPDebug1Arg(LDAP_DEBUG_ANY,
-                      "ldbm_search: instance %s does not exist.\n",
-                      inst->inst_name);
+                      "ldbm_search: instance \"%s\" does not exist.\n",
+                      inst ? inst->inst_name : "null instance");
         return( -1 );
     }
     base = slapi_sdn_get_dn(basesdn);