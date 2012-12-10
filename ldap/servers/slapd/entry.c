7af4fc1cafc1b5a7c68b7a90fd90523a18e9c71e
Noriko Hosoi <nhosoi@totoro.usersys.redhat.com>
2012-12-10 10:37:24 -0800

    Trac Ticket #531 - loading an entry from the database should use str2entry_fast
    https://fedorahosted.org/389/ticket/531
    
    Bug Description: When loading an entry from the backend, it
    is known to be well formed and str2entry_fast() could be used,
    but always str2entry_dupcheck() is called.  The reason is that
    id2entry() sets the flag SLAPI_STR2ENTRY_NO_ENTRYDN and this
    flag is not contained in the set of flags known to be handled
    by str2entry_fast() although it does have code for this flag.
    
    Fix Description: This patch is adding flags SLAPI_STR2ENTRY_
    NO_ENTRYDN and SLAPI_STR2ENTRY_DN_NORMALIZED to the local
    macro SLAPI_STRENTRY_FLAGS_HANDLED_IN_SLAPI_STR2ENTRY,
    which is used to determine the string formatted entry can be
    processed by str2entry_fast or not.  Also, this patch defines
    a macro str2entry_can_use_fast to increase the code readability.

--- ldap/servers/slapd/entry.c
+++ ldap/servers/slapd/entry.c
@@ -1328,14 +1328,24 @@ free_and_return:
 			( SLAPI_STR2ENTRY_IGNORE_STATE							\
 			| SLAPI_STR2ENTRY_EXPAND_OBJECTCLASSES					\
 			| SLAPI_STR2ENTRY_TOMBSTONE_CHECK						\
-			| SLAPI_STR2ENTRY_USE_OBSOLETE_DNFORMAT 				\
+			| SLAPI_STR2ENTRY_USE_OBSOLETE_DNFORMAT					\
+			| SLAPI_STR2ENTRY_NO_ENTRYDN							\
+			| SLAPI_STR2ENTRY_DN_NORMALIZED							\
 			)
 
 #define SLAPI_STRENTRY_FLAGS_HANDLED_BY_STR2ENTRY_FAST				\
- 			( SLAPI_STR2ENTRY_INCLUDE_VERSION_STR					\
- 			| SLAPI_STRENTRY_FLAGS_HANDLED_IN_SLAPI_STR2ENTRY		\
- 			)
+			( SLAPI_STR2ENTRY_INCLUDE_VERSION_STR					\
+			| SLAPI_STRENTRY_FLAGS_HANDLED_IN_SLAPI_STR2ENTRY		\
+			)
 
+/*
+ * If well-formed LDIF has not been provided OR if a flag that is
+ * not handled by str2entry_fast() has been passed in, call the
+ * slower but more forgiving str2entry_dupcheck() function.
+ */
+#define str2entry_can_use_fast(flags) \
+			(((flags) & SLAPI_STR2ENTRY_NOT_WELL_FORMED_LDIF) || \
+			 ((flags) & ~SLAPI_STRENTRY_FLAGS_HANDLED_BY_STR2ENTRY_FAST))
 
 Slapi_Entry *
 slapi_str2entry( char *s, int flags )
@@ -1353,17 +1363,16 @@ slapi_str2entry( char *s, int flags )
 	 * not handled by str2entry_fast() has been passed in, call the
 	 * slower but more forgiving str2entry_dupcheck() function.
 	 */
-	if ( 0 != ( flags & SLAPI_STR2ENTRY_NOT_WELL_FORMED_LDIF ) ||
-		 0 != ( flags & ~SLAPI_STRENTRY_FLAGS_HANDLED_BY_STR2ENTRY_FAST ))
-    {
+	if (str2entry_can_use_fast(flags))
+	{
 	    e= str2entry_dupcheck( NULL/*dn*/, s, flags, read_stateinfo );
-    }
-    else
-    {
+	}
+	else
+	{
 	    e= str2entry_fast( NULL/*dn*/, s, flags, read_stateinfo );
-    }
-    if (!e)
-       return e;	/* e == NULL */
+	}
+	if (!e)
+	   return e;	/* e == NULL */
 
 	if ( flags & SLAPI_STR2ENTRY_EXPAND_OBJECTCLASSES )
 	{
@@ -1415,8 +1424,7 @@ slapi_str2entry_ext( const char *normdn, char *s, int flags )
 	 * not handled by str2entry_fast() has been passed in, call the
 	 * slower but more forgiving str2entry_dupcheck() function.
 	 */
-	if ( 0 != ( flags & SLAPI_STR2ENTRY_NOT_WELL_FORMED_LDIF ) ||
-			0 != ( flags & ~SLAPI_STRENTRY_FLAGS_HANDLED_BY_STR2ENTRY_FAST ))
+	if (str2entry_can_use_fast(flags))
 	{
 	    e = str2entry_dupcheck( normdn, s, 
 	                    flags|SLAPI_STR2ENTRY_DN_NORMALIZED, read_stateinfo );
