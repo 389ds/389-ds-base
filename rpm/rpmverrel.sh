52fb682ceb0b1f03de26b6f9d56a8ee2476d9422
Nathan Kinder <nkinder@redhat.com>
2013-04-22 13:34:44 -0700

    Add git commit hash to developer rpm build names
    We need to set the srcdir variable in order for the most recent
    git hash to be determined by VERSION.sh.  This patch will result
    in deveoper RPM builds having the git commit hash in the release
    field.

--- rpm/rpmverrel.sh
+++ rpm/rpmverrel.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+# Set srcdir so VERSION.sh is able to
+# determine the last git commit hash.
+srcdir=`pwd`
+
 # Source VERSION.sh to set the version
 # and release environment variables.
 source ./VERSION.sh
