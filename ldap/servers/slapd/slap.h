ce102a9381e63b5d2a83f0abcee22ca008a84e0a
Mark Reynolds <mreynolds@redhat.com>
2013-05-09 16:58:18 -0400

    Revert "Ticket 47355 - dse.ldif doesn't replicate update to nsslapd-sasl-mapping-fallback"
    This feature was not included in 1.3.0, and should not have been cherry picked.
    
    This reverts commit 3148cf2957fa7774e0539c63e9fd9d3bf682a03c.

--- ldap/servers/slapd/slap.h
+++ ldap/servers/slapd/slap.h
@@ -2278,14 +2278,6 @@ typedef struct _slapdFrontendConfig {
   /* normalized dn cache */
   int ndn_cache_enabled;
   size_t ndn_cache_max_size;
-
-  slapi_onoff_t return_orig_type; /* if on, search returns original type set in attr list */
-
-  /* atomic settings */
-  Slapi_Counter *ignore_vattrs;
-  slapi_onoff_t sasl_mapping_fallback;
-  slapi_onoff_t unhashed_pw_switch;	/* switch to on/off/nolog unhashed pw */
-  slapi_onoff_t enable_turbo_mode;
 } slapdFrontendConfig_t;
 
 /* possible values for slapdFrontendConfig_t.schemareplace */
