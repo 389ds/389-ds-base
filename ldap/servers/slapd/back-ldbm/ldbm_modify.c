ed673bba27fab9f703db5cc44b4ffc5e8f7f7bd0
Rich Megginson <rmeggins@redhat.com>
2013-07-09 11:38:39 -0600

    Ticket #47392 - ldbm errors when adding/modifying/deleting entries
    https://fedorahosted.org/389/ticket/47392
    Reviewed by: lkrispenz (Thanks!)
    Branch: 389-ds-base-1.2.11
    Fix Description: The problem is caused by cache consistency issues with the
    RUV entry.  Before the txn starts, we grab a pointer to the RUV entry in
    the cache.  When DNA (or any betxnpreop plugin) updates the database, it
    will also grab a pointer to the cached RUV entry and modify it, out from
    under the parent txn.  This can also cause the max CSN in the RUV to go
    backwards - the nested txn will have a later CSN which will be
    overwritten by the earlier CSN from the parent txn.
    The fix is to move the ldbm_ruv_txn code inside the transaction loop after
    the betxnpreop plugins have been run.  Also have to add modify_term inside
    the retry logic to cancel the modify ruv txn stuff in order to retry.
    The other part of the fix is to tell the code that updates the max CSN
    in the RUV to skip changes that would cause the RUV to go backwards,
    and return a code to the caller that tells the caller that the CSN is
    already covered.  The code that updates the RUV for the txn will skip
    the modify operations in that case.
    Platforms tested: RHEL6 x86_64
    Flag Day: no
    Doc impact: no
    (cherry picked from commit 9bf5ac83289dcadc922a628c652aebabc4725231)
    (cherry picked from commit 42abece92028193530296b5d96a6cb5261d9aa61)
    (cherry picked from commit 4f722eff41b69dbfc17266ef6545c680b9f7c544)
    fixed up for 7p branch

--- ldap/servers/slapd/back-ldbm/ldbm_modify.c
+++ ldap/servers/slapd/back-ldbm/ldbm_modify.c
@@ -500,21 +500,7 @@ ldbm_back_modify( Slapi_PBlock *pb )
 		goto error_return;
 	}
 
-	if (!is_ruv && !is_fixup_operation && !NO_RUV_UPDATE(li)) {
-		ruv_c_init = ldbm_txn_ruv_modify_context( pb, &ruv_c );
-		if (-1 == ruv_c_init) {
-			LDAPDebug( LDAP_DEBUG_ANY,
-				"ldbm_back_modify: ldbm_txn_ruv_modify_context "
-				"failed to construct RUV modify context\n",
-				0, 0, 0);
-			ldap_result_code= LDAP_OPERATIONS_ERROR;
-			retval = 0;
-			goto error_return;
-		}
-	}
-
-	/*
-	 * Grab a copy of the mods and the entry in case the be_txn_preop changes
+	/* Grab a copy of the mods and the entry in case the be_txn_preop changes
 	 * the them.  If we have a failure, then we need to reset the mods to their
 	 * their original state;
 	 */
@@ -550,6 +536,12 @@ ldbm_back_modify( Slapi_PBlock *pb )
 			if ( (original_entry = backentry_dup( ec )) == NULL ) {
 				ldap_result_code= LDAP_OPERATIONS_ERROR;
 				goto error_return;
+			}
+
+			if (ruv_c_init) {
+				/* reset the ruv txn stuff */
+				modify_term(&ruv_c, be);
+				ruv_c_init = 0;
 			}
 
 			LDAPDebug0Args(LDAP_DEBUG_BACKLDBM,
@@ -665,6 +657,19 @@ ldbm_back_modify( Slapi_PBlock *pb )
 				goto error_return;
 			}
 
+		}
+
+		if (!is_ruv && !is_fixup_operation && !NO_RUV_UPDATE(li)) {
+			ruv_c_init = ldbm_txn_ruv_modify_context( pb, &ruv_c );
+			if (-1 == ruv_c_init) {
+				LDAPDebug( LDAP_DEBUG_ANY,
+					"ldbm_back_modify: ldbm_txn_ruv_modify_context "
+					"failed to construct RUV modify context\n",
+					0, 0, 0);
+				ldap_result_code= LDAP_OPERATIONS_ERROR;
+				retval = 0;
+				goto error_return;
+			}
 		}
 
 		if (ruv_c_init) {
