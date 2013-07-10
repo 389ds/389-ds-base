536c40b85cfdfc3e6f641ac44f7fdd6e5950d0b8
Matthew Via <matthew.via@mailtrust.com>
2013-07-10 11:30:57 -0600

    Ticket #47428 - Memory leak in 389-ds-base 1.2.11.15
    https://fedorahosted.org/389/ticket/47428
    Reviewed by: rmeggins
    Branch: 389-ds-base-1.2.11
    Fix Description: Call ber_sockbuf_remove_io to remove our openldap io layer
    from the connection c_sb and free the data associated with it.
    Platforms tested: RHEL6 x86_64
    Flag Day: no
    Doc impact: no
    (cherry picked from commit df93b03da12c22d18a4153105f687671e52efdd5)
    (cherry picked from commit ed26da0722a23a50fb286e8bc57ec4fea8dde4a2)
    (cherry picked from commit b18ee0b41fbcc901ed2a9b5c0431708e452a40c6)
    (cherry picked from commit 6e0a8efb16a1651387623ce997c021f0460d2788)
    (cherry picked from commit ef6e05286dbd4df3bc20cee51b1769496612a7af)

--- ldap/servers/slapd/connection.c
+++ ldap/servers/slapd/connection.c
@@ -263,6 +263,7 @@ connection_cleanup(Connection *conn)
 	/* destroy any sasl context */
 	sasl_dispose((sasl_conn_t**)&conn->c_sasl_conn);
 	/* PAGED_RESULTS */
+	handle_closed_connection(conn);  /* Clean up sockbufs */
 	pagedresults_cleanup(conn, 0 /* do not need to lock inside */);
 
 	/* free the connection socket buffer */
