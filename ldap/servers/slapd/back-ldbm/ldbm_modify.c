1214168a222a35627b2bb9964600fad0246558cd
Mark Reynolds <mreynolds@redhat.com>
2013-12-09 16:57:35 -0500

    Ticket 47622 - Automember betxnpreoperation - transaction not aborted when group entry does not exist
    Bug Description:  If the group defined in the automember plugin does not exist, than any add operation
                      that should trigger an update, succeeds even though the automember update failed.
    
    Fix Description:  Return an error if a automember post operation update fails - previously we always
                      returned success.
    
                      Updated plugin_call_func() to check the result of betxn postop plugins.
    
                      Also added return text to the result message when a betxn plugin fails.  This is
                      useful for clients to explain why the operation failed.
    
    https://fedorahosted.org/389/ticket/47622
    
    Jenkins: passed
    Valgrind: passed
    Coverity: passed
    
    Reviewed by: rmeggins(Thanks!)

--- ldap/servers/slapd/back-ldbm/ldbm_modify.c
+++ ldap/servers/slapd/back-ldbm/ldbm_modify.c
@@ -623,6 +623,7 @@ ldbm_back_modify( Slapi_PBlock *pb )
 			if (!opreturn) {
 				slapi_pblock_set(pb, SLAPI_PLUGIN_OPRETURN, ldap_result_code ? &ldap_result_code : &retval);
 			}
+			slapi_pblock_get(pb, SLAPI_PB_RESULT_TEXT, &ldap_result_message);
 			goto error_return;
 		}
 
@@ -793,6 +794,7 @@ ldbm_back_modify( Slapi_PBlock *pb )
 		if (!opreturn) {
 			slapi_pblock_set(pb, SLAPI_PLUGIN_OPRETURN, ldap_result_code ? &ldap_result_code : &retval);
 		}
+		slapi_pblock_get(pb, SLAPI_PB_RESULT_TEXT, &ldap_result_message);
 		goto error_return;
 	}
 
