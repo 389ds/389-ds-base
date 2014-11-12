3ce60db0a404b4663df6005b78027332d0e56f95
Mark Reynolds <mreynolds@redhat.com>
2014-11-12 10:15:46 -0500

    Ticket 47953 - Should not check aci syntax when deleting an aci
    Bug Description:  Trying to delete an aci that has an invalid sytenx, generates a
                      syntax error when trying to remove it.
    
    Fix Description:  Do not check the syntax of an aci if it's being deleted.
    
    https://fedorahosted.org/389/ticket/47953
    
    Reviewed by: ?

--- ldap/servers/plugins/acl/acl.c
+++ ldap/servers/plugins/acl/acl.c
@@ -1553,11 +1553,12 @@ acl_check_mods(
 
 				/* Are we adding/replacing a aci attribute
 				** value. In that case, we need to make
-				** sure that the new value has thr right 
+				** sure that the new value has the right
 				** syntax
 				*/
-				if (strcmp(mod->mod_type, 
-					aci_attr_type) == 0) {
+				if (!SLAPI_IS_MOD_DELETE(mod->mod_op) &&
+				    strcmp(mod->mod_type, aci_attr_type) == 0)
+				{
 					if ( 0 != (rv = acl_verify_syntax(pb, e_sdn,
 						mod->mod_bvalues[i], errbuf))) {
 						aclutil_print_err(rv, e_sdn, 
