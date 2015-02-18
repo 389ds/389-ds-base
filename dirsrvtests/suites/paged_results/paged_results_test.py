20888a606b1eabf79723f247542a17c87dcf354d
Mark Reynolds <mreynolds@redhat.com>
2015-02-18 17:32:05 -0500

    Ticket 48003 - add template scripts
    Description:  Add template scripts for most plugins/common features
    
    https://fedorahosted.org/389/ticket/48003
    
    Reviewed by: nhosoi(Thanks!)
    
    (cherry picked from commit 6dcc12ca1444889769be3341e180b236727ac857)

---
+++ dirsrvtests/suites/paged_results/paged_results_test.py
@@ -0,0 +1,85 @@
+import os
+import sys
+import time
+import ldap
+import logging
+import pytest
+from lib389 import DirSrv, Entry, tools, tasks
+from lib389.tools import DirSrvTools
+from lib389._constants import *
+from lib389.properties import *
+from lib389.tasks import *
+from lib389.utils import *
+
+logging.getLogger(__name__).setLevel(logging.DEBUG)
+log = logging.getLogger(__name__)
+
+installation1_prefix = None
+
+
+class TopologyStandalone(object):
+    def __init__(self, standalone):
+        standalone.open()
+        self.standalone = standalone
+
+
+@pytest.fixture(scope="module")
+def topology(request):
+    global installation1_prefix
+    if installation1_prefix:
+        args_instance[SER_DEPLOYED_DIR] = installation1_prefix
+
+    # Creating standalone instance ...
+    standalone = DirSrv(verbose=False)
+    args_instance[SER_HOST] = HOST_STANDALONE
+    args_instance[SER_PORT] = PORT_STANDALONE
+    args_instance[SER_SERVERID_PROP] = SERVERID_STANDALONE
+    args_instance[SER_CREATION_SUFFIX] = DEFAULT_SUFFIX
+    args_standalone = args_instance.copy()
+    standalone.allocate(args_standalone)
+    instance_standalone = standalone.exists()
+    if instance_standalone:
+        standalone.delete()
+    standalone.create()
+    standalone.open()
+
+    # Clear out the tmp dir
+    standalone.clearTmpDir(__file__)
+
+    return TopologyStandalone(standalone)
+
+
+def test_paged_results_init(topology):
+    '''
+    Write any test suite initialization here(if needed)
+    '''
+
+    return
+
+
+def test_paged_results_(topology):
+    '''
+    Write a single test here...
+    '''
+
+    return
+
+
+def test_paged_results_final(topology):
+    topology.standalone.delete()
+    log.info('paged_results test suite PASSED')
+
+
+def run_isolated():
+    global installation1_prefix
+    installation1_prefix = None
+
+    topo = topology(True)
+    test_paged_results_init(topo)
+    test_paged_results_(topo)
+    test_paged_results_final(topo)
+
+
+if __name__ == '__main__':
+    run_isolated()
+
