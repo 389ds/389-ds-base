1e736a7c04e7a5bbd649b9e28a1630f0167d0c1f
Mark Reynolds <mreynolds@redhat.com>
2015-08-05 16:31:49 -0400

    Ticket 48215 - verify_db.pl doesn't verify DB specified by -a option
    Bug Description:  verify_db.pl -a only uses the db location for
                      checking the transaction logs, because it ends up
                      calling "nsslapd dbverify" which only checks the
                      db files in the server configuration.
    
    Fix Description:  Allow a new argument to be passed to "nsslapd dbverify"
                      that specifies the db parent directory.
    
    https://fedorahosted.org/389/ticket/48215
    
    Reviewed by: nhosoi(Thanks!)
    
    (cherry picked from commit 27fadb75ec1f3b252028ce715cd7fa16da1f6525)
    
    Conflicts:
    	ldap/servers/slapd/pblock.c
    
    (cherry picked from commit 6878a6cb355ff96fc390d71528978420ffc316e7)

--- ldap/servers/slapd/pblock.c
+++ ldap/servers/slapd/pblock.c
@@ -1700,6 +1700,11 @@ slapi_pblock_get( Slapi_PBlock *pblock, int arg, void *value )
 		(*(IFP*)value) = pblock->pb_txn_ruv_mods_fn;
 		break;
 
+	/* dbverify */
+	case SLAPI_DBVERIFY_DBDIR:
+		(*(char **)value) = pblock->pb_dbverify_dbdir;
+		break;
+
 	/* Search results set */
 	case SLAPI_SEARCH_RESULT_SET:
 		if(pblock->pb_op!=NULL)
@@ -3535,6 +3540,11 @@ slapi_pblock_set( Slapi_PBlock *pblock, int arg, void *value )
 	/* ACI Target Check */
 	case SLAPI_ACI_TARGET_CHECK:
 		pblock->pb_aci_target_check = *((int *) value);
+		break;
+
+	/* dbverify */
+	case SLAPI_DBVERIFY_DBDIR:
+		pblock->pb_dbverify_dbdir = (char *) value;
 		break;
 
 	default:
