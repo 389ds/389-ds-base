26613cf3d1dbca136a43e15ee5d6a20f3641b362
Stanislav Zidek <szidek@redhat.com>
2015-10-21 17:58:31 -0700

    Ticket #48289 - 389-ds-base: ldclt-bin killed by SIGSEGV
    Description: If NULL source string was passed to parseFilter, it caused
    NULL dereference.
    
    https://fedorahosted.org/389/ticket/48289
    
    Reviewed by nhosoi@redhat.com.
    
    (cherry picked from commit 5b33c781d2568c930a0856f0a42c1e1d53e3358f)
    (cherry picked from commit 8cd610a944daf72a41e5b35065f8b363f8154f28)
    (cherry picked from commit 49d0ef667441e009af071fd0f4d48f31f3514063)

--- ldap/servers/slapd/tools/ldclt/ldclt.c
+++ ldap/servers/slapd/tools/ldclt/ldclt.c
@@ -1225,6 +1225,11 @@ parseFilter (
 {
   int	 i, j;
 
+  if (!src) {
+    printf ("Error: NULL source string is passed.\n");
+    return (-1);
+  }
+
   for (i=0 ; (i<strlen(src)) && (src[i]!='X') ; i++);
   *head = (char *)malloc(i+1);
   if (*head == NULL)
