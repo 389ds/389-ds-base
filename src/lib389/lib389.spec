260adce00e292e7e120e16c21bae924ca9393169
Mark Reynolds <mreynolds@redhat.com>
2015-12-02 10:51:51 -0500

    Ticket 48358 - Prepare lib389 for Fedora Packaging
    Description:  Created spec file, and updated setup.py/setup.cfg.  Also
                  restructured the code layout to all be under /lib389
    
    https://fedorahosted.org/389/ticket/48358
    
    Reviewed by: spichugi(Thanks!)

---
+++ src/lib389/lib389.spec
@@ -0,0 +1,65 @@
+%{!?__python2: %global __python2 %__python}
+%{!?python2_sitelib: %global python2_sitelib %(%{__python2} -c "from distutils.sysconfig import get_python_lib; print(get_python_lib())")}
+
+%define name lib389
+%define version 1.0.1
+%define release 1
+
+Summary: A library for accessing, testing, and configuring the 389 Directory Server
+Name: %{name}
+Version: %{version}
+Release: %{release}%{?dist}
+Source0: %{name}-%{version}.tar.bz2
+License: GPLv3+
+Group: Development/Libraries
+#BuildRoot: %{_tmppath}/%{name}-%{version}-%{release}-buildroot
+BuildRoot: %{_tmppath}/%{name}-%{version}-%{release}-root-%(%{__id_u} -n)
+Prefix: %{_prefix}
+BuildArch: noarch
+Vendor: Red Hat Inc. <389-devel@lists.fedoraproject.org>
+Url: http://port389.org/wiki/Upstream_test_framework
+Requires: python-ldap pytest python-krbV
+
+# Currently python-ldap is not python3 compatible, so lib389 only works with 
+# python 2.7
+
+%description
+This repository contains tools and libraries for accessing, testing, and
+configuring the 389 Directory Server.
+
+%prep
+%setup -qc
+mv %{name}-%{version} python2
+
+%build
+pushd python2
+# Remove CFLAGS=... for noarch packages (unneeded)
+CFLAGS="$RPM_OPT_FLAGS" %{__python2} setup.py build
+popd
+
+%install
+rm -rf $RPM_BUILD_ROOT
+pushd python2
+%{__python2} setup.py install -O1 --skip-build --root $RPM_BUILD_ROOT
+popd
+
+%check
+pushd python2
+%{__python2} setup.py test
+popd
+
+%clean
+rm -rf $RPM_BUILD_ROOT
+
+%files
+%defattr(-,root,root,-)
+%doc
+%{python2_sitelib}/*
+
+%changelog
+* Tue Dec 1 2015 Mark Reynolds <mreynolds@redhat.com> - 1.0.1-1
+- Initial Fedora Package
+
+
+
+
--- src/lib389/setup.py
+++ src/lib389/setup.py
@@ -26,28 +26,27 @@ with open(path.join(here, 'README'), 'r') as f:
 
 setup(
     name='lib389',
+    license='GPLv3+',
     version=version,
-    description='A library for accessing 389 directory server',
+    description='A library for accessing and configuring the 389 Directory ' +
+                'Server',
     long_description=long_description,
-
     url='http://port389.org/wiki/Upstream_test_framework',
 
     author='Red Hat Inc.',
     author_email='389-devel@lists.fedoraproject.org',
 
-        classifiers=[
-            'Development Status :: 3 - Alpha',
-            'Intended Audience :: Developers',
-            'Operating System :: POSIX :: Linux',
-            'Programming Language :: Python :: 2',
-            'Programming Language :: Python :: 2.6',
-            'Programming Language :: Python :: 2.7',
-            'Topic :: Software Development :: Libraries',
-            'Topic :: Software Development :: Quality Assurance',
-            'Topic :: Software Development :: Testing'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 2 :: Only',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Quality Assurance',
+        'Topic :: Software Development :: Testing'],
 
-    keywords='389 directory server test',
+    keywords='389 directory server test configure',
     packages=find_packages(exclude=['tests*']),
-
     install_requires=['python-ldap', 'pytest', 'python-krbV'],
 )
