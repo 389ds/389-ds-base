201ab483239a199d5ad98fbce1c8662d08edd772
William Brown <firstyear@redhat.com>
2016-05-16 12:55:09 +1000

    Ticket 48434 - lib389 logging tools
    Bug Description:  It would be useful to be able to search all logs of the server
    even rotated ones.
    
    Fix Description:  This adds the _archive methods which allows the searching of
    older, rotated logs.
    
    https://fedorahosted.org/389/ticket/48434
    
    Author: wibrown
    
    Review by: mreynolds (Thanks!)

--- src/lib389/lib389/dirsrv_log.py
+++ src/lib389/lib389/dirsrv_log.py
@@ -10,22 +10,62 @@
 """
 
 import re
+import gzip
+from datetime import datetime
+from dateutil.parser import parse as dt_parse
+from glob import glob
 from lib389._constants import DN_CONFIG
 from lib389.properties import LOG_ACCESS_PATH, LOG_ERROR_PATH
 
 # Because many of these settings can change live, we need to check for certain
 # attributes all the time.
 
+MONTH_LOOKUP = {
+    'Jan' : 1,
+    'Feb' : 2,
+    'Mar' : 3,
+    'Apr' : 4,
+    'May' : 5,
+    'Jun' : 6,
+    'Jul' : 7,
+    'Aug' : 8,
+    'Oct' : 9,
+    'Sep' : 10,
+    'Nov' : 11,
+    'Dec' : 12,
+}
+
 class DirsrvLog(object):
     def __init__(self, dirsrv):
         self.dirsrv = dirsrv
         self.log = self.dirsrv.log
+        self.prog_timestamp = re.compile('\[(?P<day>\d*)\/(?P<month>\w*)\/(?P<year>\d*):(?P<hour>\d*):(?P<minute>\d*):(?P<second>\d*)(.(?P<nanosecond>\d*))+\s(?P<tz>\+\d*)')
 
     def _get_log_attr(self, attr):
         return self.dirsrv.getEntry(DN_CONFIG).__getattr__(attr)
 
     def _get_log_path(self):
         return self._get_log_attr(self.log_path_attr)
+
+    def _get_all_log_paths(self):
+        return glob("%s.*-*" % self._get_log_path() ) + [self._get_log_path()]
+
+    def readlines_archive(self):
+        """
+        Returns an array of all the lines in all logs, included rotated logs
+        and compressed logs. (gzip)
+        Will likely be very slow. Try using match instead.
+        """
+        lines = []
+        for log in self._get_all_log_paths():
+            # Open the log
+            if log.endswith('.gz'):
+                with gzip.open(log, 'r') as lf:
+                    lines += lf.readlines()
+            else:
+                with open(log, 'r') as lf:
+                    lines += lf.readlines()
+        return lines
 
     def readlines(self):
         """
@@ -38,8 +78,25 @@ class DirsrvLog(object):
             # Open the log
             with open(self.lpath, 'r') as lf:
                 lines = lf.readlines()
-            # return a readlines fn?
         return lines
+
+    def match_archive(self, pattern):
+        results = []
+        prog = re.compile(pattern)
+        for log in self._get_all_log_paths():
+            if log.endswith('.gz'):
+                with gzip.open(log, 'r') as lf:
+                    for line in lf:
+                        mres = prog.match(line)
+                        if mres:
+                            results.append(line)
+            else:
+                with open(log, 'r') as lf:
+                    for line in lf:
+                        mres = prog.match(line)
+                        if mres:
+                            results.append(line)
+        return results
 
     def match(self, pattern):
         results = []
@@ -51,8 +108,23 @@ class DirsrvLog(object):
                     mres = prog.match(line)
                     if mres:
                         results.append(line)
-        ## do a read lines, then match with the associated RE.
         return results
+
+    def parse_timestamp(self, ts):
+        timedata = self.prog_timestamp.match(ts).groupdict()
+        # Now, have to convert month to an int.
+        dt_str = '{YEAR}-{MONTH}-{DAY} {HOUR}-{MINUTE}-{SECOND} {TZ}'.format(
+            YEAR=timedata['year'],
+            MONTH=MONTH_LOOKUP[timedata['month']],
+            DAY=timedata['day'],
+            HOUR=timedata['hour'],
+            MINUTE=timedata['minute'],
+            SECOND=timedata['second'],
+            TZ=timedata['tz'],
+            )
+        dt = dt_parse(dt_str)
+        dt = dt.replace(microsecond= int(timedata['nanosecond']) / 1000)
+        return dt
 
 class DirsrvAccessLog(DirsrvLog):
     def __init__(self, dirsrv):
@@ -89,6 +161,8 @@ class DirsrvAccessLog(DirsrvLog):
             action.update(discon.groupdict())
             action['action'] = 'DISCONNECT'
 
+        action['datetime'] = self.parse_timestamp(action['timestamp'])
+
         if self.dirsrv.verbose:
             self.log.info(action)
         return action
@@ -106,7 +180,11 @@ class DirsrvErrorLog(DirsrvLog):
 
     def parse_line(self, line):
         line = line.strip()
-        return self.prog_m1.match(line).groupdict()
+        action = self.prog_m1.match(line).groupdict()
 
-    def parse_lines(self, line):
+        action['datetime'] = self.parse_timestamp(action['timestamp'])
+        return action
+
+
+    def parse_lines(self, lines):
         return map(self.parse_line, lines)
--- src/lib389/lib389/tests/dirsrv_log_test.py
+++ src/lib389/lib389/tests/dirsrv_log_test.py
@@ -10,6 +10,9 @@ from lib389._constants import *
 from lib389 import DirSrv, Entry
 import pytest
 import time
+import shutil
+import datetime
+from dateutil.tz import tzoffset
 
 INSTANCE_PORT = 54321
 INSTANCE_SERVERID = 'loggingtest'
@@ -25,7 +28,7 @@ class TopologyStandalone(object):
 def topology(request):
     standalone = DirSrv(verbose=False)
     standalone.log.debug("Instance allocated")
-    args = {SER_HOST: LOCALHOST,
+    args = {
             SER_PORT: INSTANCE_PORT,
             # SER_DEPLOYED_DIR:  INSTANCE_PREFIX,
             SER_SERVERID_PROP: INSTANCE_SERVERID}
@@ -34,18 +37,32 @@ def topology(request):
         standalone.delete()
     standalone.create()
     standalone.open()
+    standalone.config.set('nsslapd-accesslog-logbuffering', 'off')
 
     def fin():
         standalone.delete()
     request.addfinalizer(fin)
+    # We have to wait for time to elapse for the access log to be flushed.
 
     return TopologyStandalone(standalone)
+
+def test_access_log_rotation(topology):
+    """
+    Check we can parse rotated logs as well as active log.
+    """
+
+    # Artificially rotate the log.
+    lpath = topology.standalone.ds_access_log._get_log_path()
+    shutil.copyfile(lpath, '%s.20160515-104822' % lpath  )
+    # check we have the right number of lines.
+    access_lines = topology.standalone.ds_access_log.readlines_archive()
+    assert(len(access_lines) > 0)
+    access_lines = topology.standalone.ds_access_log.match_archive('.*fd=.*')
+    assert(len(access_lines) > 0)
 
 
 def test_access_log(topology):
     """Check the parsing of the access log"""
-    # We have to wait for time to elapse for the access log to be flushed.
-    time.sleep(60)
     access_lines = topology.standalone.ds_access_log.readlines()
     assert(len(access_lines) > 0)
     access_lines = topology.standalone.ds_access_log.match('.*fd=.*')
@@ -53,15 +70,15 @@ def test_access_log(topology):
     # Test the line parser in a basic way.
     assert(
         topology.standalone.ds_access_log.parse_line('[27/Apr/2016:12:49:49.726093186 +1000] conn=1 fd=64 slot=64 connection from ::1 to ::1') ==
-        {'slot': '64', 'remote': '::1', 'action': 'CONNECT', 'timestamp': '[27/Apr/2016:12:49:49.726093186 +1000]', 'fd': '64', 'conn': '1', 'local': '::1'}
+        {'slot': '64', 'remote': '::1', 'action': 'CONNECT', 'timestamp': '[27/Apr/2016:12:49:49.726093186 +1000]', 'fd': '64', 'conn': '1', 'local': '::1', 'datetime': datetime.datetime(2016, 4, 27, 12, 0, 0, 726093, tzinfo=tzoffset(None, 36000))}
     )    
     assert(
         topology.standalone.ds_access_log.parse_line('[27/Apr/2016:12:49:49.727235997 +1000] conn=1 op=2 SRCH base="cn=config" scope=0 filter="(objectClass=*)" attrs="nsslapd-instancedir nsslapd-errorlog nsslapd-accesslog nsslapd-auditlog nsslapd-certdir nsslapd-schemadir nsslapd-bakdir nsslapd-ldifdir"') ==
-        {'rem': 'base="cn=config" scope=0 filter="(objectClass=*)" attrs="nsslapd-instancedir nsslapd-errorlog nsslapd-accesslog nsslapd-auditlog nsslapd-certdir nsslapd-schemadir nsslapd-bakdir nsslapd-ldifdir"', 'action': 'SRCH', 'timestamp': '[27/Apr/2016:12:49:49.727235997 +1000]', 'conn': '1', 'op': '2'}
+        {'rem': 'base="cn=config" scope=0 filter="(objectClass=*)" attrs="nsslapd-instancedir nsslapd-errorlog nsslapd-accesslog nsslapd-auditlog nsslapd-certdir nsslapd-schemadir nsslapd-bakdir nsslapd-ldifdir"', 'action': 'SRCH', 'timestamp': '[27/Apr/2016:12:49:49.727235997 +1000]', 'conn': '1', 'op': '2', 'datetime': datetime.datetime(2016, 4, 27, 12, 0, 0, 727235, tzinfo=tzoffset(None, 36000))}
     )
     assert(
         topology.standalone.ds_access_log.parse_line('[27/Apr/2016:12:49:49.736297002 +1000] conn=1 op=4 fd=64 closed - U1') ==
-        {'status': 'U1', 'fd': '64', 'action': 'DISCONNECT', 'timestamp': '[27/Apr/2016:12:49:49.736297002 +1000]', 'conn': '1', 'op': '4'}
+        {'status': 'U1', 'fd': '64', 'action': 'DISCONNECT', 'timestamp': '[27/Apr/2016:12:49:49.736297002 +1000]', 'conn': '1', 'op': '4', 'datetime': datetime.datetime(2016, 4, 27, 12, 0, 0, 736297, tzinfo=tzoffset(None, 36000))}
     )
 
 def test_error_log(topology):
@@ -74,7 +91,7 @@ def test_error_log(topology):
 
     assert(
         topology.standalone.ds_error_log.parse_line('[27/Apr/2016:13:46:35.775670167 +1000] slapd started.  Listening on All Interfaces port 54321 for LDAP requests') ==
-        {'timestamp': '[27/Apr/2016:13:46:35.775670167 +1000]', 'message': 'slapd started.  Listening on All Interfaces port 54321 for LDAP requests'}
+        {'timestamp': '[27/Apr/2016:13:46:35.775670167 +1000]', 'message': 'slapd started.  Listening on All Interfaces port 54321 for LDAP requests', 'datetime': datetime.datetime(2016, 4, 27, 13, 0, 0, 775670, tzinfo=tzoffset(None, 36000))}
     )
 
 
