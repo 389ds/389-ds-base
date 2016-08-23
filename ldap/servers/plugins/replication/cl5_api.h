f4df06b4d50edbf4a8e3c0a8a8ecc08a174ce870
Mark Reynolds <mreynolds@redhat.com>
2016-08-23 12:06:30 -0400

    Ticket 48964 - cleanAllRUV changelog purging incorrectly  processes all backends
    Bug Description:  When the changelog was being purged of "cleaned" rids it was checking
                      all the backend changelogs, and not the one from which the
                      cleanAllRUV task originated from.  This could corrupt a different
                      backend's changelog if both backends used the same RID.
    
    Fix Description:  Purge the changelog associated with the backend that is specified in
                      the cleanAllRUV task.  Also moved the "purging" to its own function,
                      and fixed a few compiler warnings.
    
    https://fedorahosted.org/389/ticket/48965
    
    Reviewed by: nhosoi(Thanks!)
    
    (cherry picked from commit fda00435a7536c1ded72bb78a975f3370d09a3be)

--- ldap/servers/plugins/replication/cl5_api.h
+++ ldap/servers/plugins/replication/cl5_api.h
@@ -496,6 +496,6 @@ int cl5WriteRUV();
 int cl5DeleteRUV();
 void cl5CleanRUV(ReplicaId rid);
 void cl5NotifyCleanup(int rid);
-void trigger_cl_purging(ReplicaId rid);
+void trigger_cl_purging(Replica *replica);
 
 #endif
