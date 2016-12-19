64b1ebffe5af118965bcdf3a84d62c0fc3efd196
Mark Reynolds <mreynolds@redhat.com>
2016-12-19 12:26:59 -0500

    Ticket 49071 - Import with duplicate DNs throws unexpected errors
    Bug Description:  When an import fails there are unable to flush error
                      messages.
    
    Fix Description:  When an import fails close the database files before
                      deleting them.
    
                      Also fixed a small issue in DSUtil where we did not properly
                      check if an entry was valid.
    
    https://fedorahosted.org/389/ticket/49071
    
    Reviewed by: mreynolds(one line commit rule)

--- ldap/servers/slapd/back-ldbm/import.c
+++ ldap/servers/slapd/back-ldbm/import.c
@@ -1485,12 +1485,12 @@ error:
         }
     }
     if (0 != ret) {
+        dblayer_instance_close(job->inst->inst_be);
         if (!(job->flags & (FLAG_DRYRUN|FLAG_UPGRADEDNFORMAT_V1))) {
             /* If not dryrun NOR upgradedn space */
             /* if running in the dry run mode, don't touch the db */
             dblayer_delete_instance_dir(be);
         }
-        dblayer_instance_close(job->inst->inst_be);
     } else {
         if (0 != (ret = dblayer_instance_close(job->inst->inst_be)) ) {
             import_log_notice(job, SLAPI_LOG_WARNING, "import_main_offline", "Failed to close database");
