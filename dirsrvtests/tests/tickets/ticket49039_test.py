0ebc21e8204c215dc60179442bd74cea4f9aff82
Mark Reynolds <mreynolds@redhat.com>
2017-04-05 11:05:28 -0400

    Issue 49210 - Fix regression when checking is password min  age should be checked
    Bug Description:  If a plugin makes an internal modification of userpassword
                      the connection structure in the pblock is null, and it was
                      being dereferenced.
    
    Fix Description:  These internal operations do not need to have the password
                      policy checks done.  They are intended to be unrestricted.
                      So we only need to check password policy on client connections.
                      The fix frist hecks if the connection structy is present,
                      only then it continues.
    
                      Revised test script to include the tool: ldappasswd
    
    https://pagure.io/389-ds-base/issue/49210
    
    Reviewed by: firstyear(Thanks!)
    
    (cherry picked from commit f5e4b8a39c6afca9363ea9f1239fb3b43ad21d30)

--- dirsrvtests/tests/tickets/ticket49039_test.py
+++ dirsrvtests/tests/tickets/ticket49039_test.py
@@ -2,12 +2,14 @@ import time
 import ldap
 import logging
 import pytest
+import os
 from lib389 import Entry
 from lib389._constants import *
 from lib389.properties import *
 from lib389.tasks import *
 from lib389.utils import *
 from lib389.topologies import topology_st as topo
+
 
 DEBUGGING = os.getenv("DEBUGGING", default=False)
 if DEBUGGING:
@@ -19,10 +21,38 @@ log = logging.getLogger(__name__)
 USER_DN = 'uid=user,dc=example,dc=com'
 
 
+def ssl_init(topo):
+    """ Setup TLS
+    """
+    topo.standalone.stop()
+    # Prepare SSL but don't enable it.
+    for f in ('key3.db', 'cert8.db', 'key4.db', 'cert9.db', 'secmod.db', 'pkcs11.txt'):
+        try:
+            os.remove("%s/%s" % (topo.standalone.confdir, f))
+        except:
+            pass
+    assert(topo.standalone.nss_ssl.reinit() is True)
+    assert(topo.standalone.nss_ssl.create_rsa_ca() is True)
+    assert(topo.standalone.nss_ssl.create_rsa_key_and_cert() is True)
+    # Start again
+    topo.standalone.start()
+    topo.standalone.rsa.create()
+    topo.standalone.config.set('nsslapd-ssl-check-hostname', 'off')
+    topo.standalone.config.set('nsslapd-secureport', '%s' %
+                               SECUREPORT_STANDALONE1)
+    topo.standalone.config.set('nsslapd-security', 'on')
+    topo.standalone.restart()
+
+    log.info("SSL setup complete\n")
+
+
 def test_ticket49039(topo):
     """Test "password must change" verses "password min age".  Min age should not
     block password update if the password was reset.
     """
+
+    # Setup SSL (for ldappasswd test)
+    ssl_init(topo)
 
     # Configure password policy
     try:
@@ -66,6 +96,38 @@ def test_ticket49039(topo):
         topo.standalone.modify_s(USER_DN, [(ldap.MOD_REPLACE, 'userpassword', PASSWORD)])
     except ldap.LDAPError as e:
         log.fatal('Failed to change password: error ' + e.message['desc'])
+        assert False
+
+    ###################################
+    # Make sure ldappasswd also works
+    ###################################
+
+    # Reset password as RootDN
+    try:
+        topo.standalone.simple_bind_s(DN_DM, PASSWORD)
+    except ldap.LDAPError as e:
+        log.fatal('Failed to bind as rootdn: error ' + e.message['desc'])
+        assert False
+
+    try:
+        topo.standalone.modify_s(USER_DN, [(ldap.MOD_REPLACE, 'userpassword', PASSWORD)])
+    except ldap.LDAPError as e:
+        log.fatal('Failed to bind: error ' + e.message['desc'])
+        assert False
+
+    time.sleep(1)
+
+    # Run ldappasswd as the User.
+    cmd = ('LDAPTLS_REQCERT=never LDAPTLS_CACERTDIR=' + topo.standalone.get_cert_dir() +
+           ' ldappasswd' + ' -h ' + topo.standalone.host + ' -Z -p 38901 -D ' + USER_DN +
+           ' -w password -a password -s password2 ' + USER_DN)
+    os.system(cmd)
+    time.sleep(1)
+
+    try:
+        topo.standalone.simple_bind_s(USER_DN, "password2")
+    except ldap.LDAPError as e:
+        log.fatal('Failed to bind: error ' + e.message['desc'])
         assert False
 
     log.info('Test Passed')
