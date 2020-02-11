81ff1dadb0debfe1a64829a7bf300f3bc67bf8bc
Ludwig Krispenz <lkrispen@redhat.com>
2020-02-11 09:47:45 +0100

    Ticket - 49623-cont cenotaph errors on modrdn operations
    Bug: In modrdn operations a cenotaph entries are created to track the time when
    	an entry had existed. But in cases where rentries were renamed in cycles
    	reusing the dns again and again this failed with an error: "faild to add cenotaph"
    
    Fix: Previous versions of cenotaphs with the same dn are not used (or maybe in very unlikely
    	scenarios) so there is no need to change the dn construction to be able to keep all
    	versions of the same cenotaph. Instead, if the creation of the cenotaph fails because
    	it already exists, the existin cenotaph is moodified with the lifespan data of the
    	cenotaph that was tried to add.
    
    Reviewed by: Thierry, thanks

---
+++ dirsrvtests/tests/tickets/ticket49623_2_test.py
@@ -0,0 +1,66 @@
+# --- BEGIN COPYRIGHT BLOCK ---
+# Copyright (C) 2020 Red Hat, Inc.
+# All rights reserved.
+#
+# License: GPL (version 3 or any later version).
+# See LICENSE for details.
+# --- END COPYRIGHT BLOCK ---
+#
+import os
+import ldap
+import pytest
+import subprocess
+from lib389.tasks import *
+from lib389.utils import *
+from lib389.topologies import topology_m1
+from lib389.idm.user import UserAccounts
+from lib389._constants import DEFAULT_SUFFIX
+from contextlib import contextmanager
+
+pytestmark = pytest.mark.tier1
+
+logging.getLogger(__name__).setLevel(logging.DEBUG)
+log = logging.getLogger(__name__)
+
+
+@pytest.mark.ds49623
+@pytest.mark.bz1790986
+def test_modrdn_loop(topology_m1):
+    """Test that renaming the same entry multiple times reusing the same
+       RDN multiple times does not result in cenotaph error messages
+
+    :id: 631b2be9-5c03-44c7-9853-a87c923d5b30
+
+    :setup: Single master instance
+
+    :steps: 1. Add an entry with RDN start rdn
+            2. Rename the entry to rdn change
+            3. Rename the entry to start again
+            4. Rename the entry to rdn change
+            5. check for cenotaph error messages
+    :expectedresults:
+            1. No error messages
+    """
+
+    topo = topology_m1.ms['master1']
+    TEST_ENTRY_RDN_START = 'start'
+    TEST_ENTRY_RDN_CHANGE = 'change'
+    TEST_ENTRY_NAME = 'tuser'
+    users = UserAccounts(topo, DEFAULT_SUFFIX)
+    user_properties = {
+        'uid': TEST_ENTRY_RDN_START,
+        'cn': TEST_ENTRY_NAME,
+        'sn': TEST_ENTRY_NAME,
+        'uidNumber': '1001',
+        'gidNumber': '2001',
+        'homeDirectory': '/home/{}'.format(TEST_ENTRY_NAME)
+    }
+
+    tuser = users.create(properties=user_properties)
+    tuser.rename('uid={}'.format(TEST_ENTRY_RDN_CHANGE), newsuperior=None, deloldrdn=True)
+    tuser.rename('uid={}'.format(TEST_ENTRY_RDN_START), newsuperior=None, deloldrdn=True)
+    tuser.rename('uid={}'.format(TEST_ENTRY_RDN_CHANGE), newsuperior=None, deloldrdn=True)
+
+    log.info("Check the log messages for cenotaph error")
+    error_msg = ".*urp_fixup_add_cenotaph - failed to add cenotaph, err= 68"
+    assert not topo.ds_error_log.match(error_msg)
