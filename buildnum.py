b32c74556ec717baef6c3bb3f64f755b38ad015d
Mark Reynolds <mreynolds@redhat.com>
2020-03-16 11:42:33 -0400

    Issue 50954 - buildnum.py - fix date formatting issue
    Description:  Use time.strftime to generate build number
                  (Thanks Matus for the suggestion)
    
    relates: https://pagure.io/389-ds-base/issue/50954
    
    Reviewed by: mhonek(Thanks!)

--- buildnum.py
+++ buildnum.py
@@ -14,15 +14,10 @@ import time
 
 SDE = os.getenv('SOURCE_DATE_EPOCH')
 if SDE is not None:
-    obj = time.gmtime(int(SDE))
+    gmtime_obj = time.gmtime(int(SDE))
 else:
-    obj = time.gmtime()
+    gmtime_obj = time.gmtime()
     
-year = obj[0]
-doy = obj[7]
-if doy < 100:
-    doy = "0" + str(doy)
-tod = str(obj[3]) + str(obj[4])
-buildnum = f"{year}.{doy}.{tod}"
-
+# Print build number
+buildnum = time.strftime("%Y.%j.%H%M", gmtime_obj)
 print(f'\\"{buildnum}\\"', end = '')
