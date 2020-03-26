324b5b1d07cda032a64f87ad98d505d427c43f10
Simon Pichugin <spichugi@redhat.com>
2020-03-26 19:33:47 +0100

    Issue 50984 - Memory leaks in disk monitoring
    Description: Memory leaks are reported by the disk monitoring test suite.
    The direct leak is related to char **dirs array which is not freed at all.
    Free the array when we clean up or go to shutdown.
    
    https://pagure.io/389-ds-base/issue/50984
    
    Reviewed by: firstyear, tbordaz (Thanks!)

--- ldap/servers/slapd/daemon.c
+++ ldap/servers/slapd/daemon.c
@@ -501,8 +501,7 @@ disk_monitoring_thread(void *nothing __attribute__((unused)))
             slapi_log_err(SLAPI_LOG_ALERT, "disk_monitoring_thread",
                     "Disk space is critically low on disk (%s), remaining space: %" PRIu64 " Kb.  Signaling slapd for shutdown...\n",
                     dirstr, (disk_space / 1024));
-            g_set_shutdown(SLAPI_SHUTDOWN_DISKFULL);
-            return;
+            goto cleanup;
         }
         /*
          *  If we are low, see if we are using verbose error logging, and turn it off
@@ -570,6 +569,8 @@ disk_monitoring_thread(void *nothing __attribute__((unused)))
             now = start;
             while ((now - start) < grace_period) {
                 if (g_get_shutdown()) {
+                    slapi_ch_array_free(dirs);
+                    dirs = NULL;
                     return;
                 }
                 /*
@@ -612,8 +613,7 @@ disk_monitoring_thread(void *nothing __attribute__((unused)))
                     slapi_log_err(SLAPI_LOG_ALERT, "disk_monitoring_thread",
                             "Disk space is critically low on disk (%s), remaining space: %" PRIu64 " Kb.  Signaling slapd for shutdown...\n",
                             dirstr, (disk_space / 1024));
-                    g_set_shutdown(SLAPI_SHUTDOWN_DISKFULL);
-                    return;
+                    goto cleanup;
                 }
                 now = slapi_current_utc_time();
             }
@@ -630,11 +630,14 @@ disk_monitoring_thread(void *nothing __attribute__((unused)))
             slapi_log_err(SLAPI_LOG_ALERT, "disk_monitoring_thread",
                     "Disk space is still too low (%" PRIu64 " Kb).  Signaling slapd for shutdown...\n",
                     (disk_space / 1024));
-            g_set_shutdown(SLAPI_SHUTDOWN_DISKFULL);
-
-            return;
+            goto cleanup;
         }
     }
+    cleanup:
+        slapi_ch_array_free(dirs);
+        dirs = NULL;
+        g_set_shutdown(SLAPI_SHUTDOWN_DISKFULL);
+        return;
 }
 
 static void
