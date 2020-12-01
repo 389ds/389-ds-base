33f60c887ffc2235f4936d18cc53b7a85fe83997
Barbora Simonova <bsmejkal@redhat.com>
2020-12-01 12:14:52 +0100

    Issue 4284 - dsidm fails to delete an organizationalUnit entry
    Description:
    Created test for dsidm organizationalunit delete and moved the function
    check_value_in_log_and_reset() to __init__.py, because it will be used for
    other dsidm tests.
    Also modified the delete() function in lib389 to be able to delete the entry
    without warning message for test purposes.
    
    Relates: https://github.com/389ds/389-ds-base/issues/4284
    
    Reviewed by: droideck (Thanks!)

--- dirsrvtests/tests/suites/clu/dsidm_account_test.py
+++ dirsrvtests/tests/suites/clu/dsidm_account_test.py
@@ -19,34 +19,12 @@ from lib389.topologies import topology_st
 from lib389.cli_base import FakeArgs
 from lib389.utils import ds_is_older
 from lib389.idm.user import nsUserAccounts
+from . import check_value_in_log_and_reset
 
 pytestmark = pytest.mark.tier0
 
 logging.getLogger(__name__).setLevel(logging.DEBUG)
 log = logging.getLogger(__name__)
-
-
-def check_value_and_reset(topology, content_list=None, content_list2=None, check_value=None, check_value_not=None):
-        if content_list2 is not None:
-            log.info('Check if content is present in output')
-            for item in content_list + content_list2:
-                assert topology.logcap.contains(item)
-
-        if content_list is not None:
-            log.info('Check if content is present in output')
-            for item in content_list:
-                assert topology.logcap.contains(item)
-
-        if check_value is not None:
-            log.info('Check if value is present in output')
-            assert topology.logcap.contains(check_value)
-
-        if check_value_not is not None:
-            log.info('Check if value is not present in output')
-            assert not topology.logcap.contains(check_value_not)
-
-        log.info('Reset the log for next test')
-        topology.logcap.flush()
 
 
 @pytest.fixture(scope="function")
