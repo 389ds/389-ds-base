89367c674fca01f87dc140925e91f74a2c5e1bba
Viktor Ashirov <vashirov@redhat.com>
2021-01-08 15:09:06 +0100

    Issue 4219 - Log internal unindexed searches (notes=A)
    Description:
    Add a test case.
    
    Relates: https://github.com/389ds/389-ds-base/issues/4219
    
    Reviewed by: @mreynolds389, @droideck, @tmihinto (Thanks!)

--- dirsrvtests/tests/suites/indexes/regression_test.py
+++ dirsrvtests/tests/suites/indexes/regression_test.py
@@ -12,13 +12,74 @@ import pytest
 import ldap
 from lib389._constants import DEFAULT_BENAME, DEFAULT_SUFFIX
 from lib389.index import Indexes
-from lib389.backend import Backends
+from lib389.backend import Backends, DatabaseConfig
 from lib389.idm.user import UserAccounts
+from lib389.idm.group import Groups, Group
 from lib389.topologies import topology_st as topo
 from lib389.utils import ds_is_older
+from lib389.plugins import MemberOfPlugin
 
 pytestmark = pytest.mark.tier1
 
+
+@pytest.fixture(scope="function")
+def add_a_group_with_users(request, topo):
+    """
+    Add a group and users, which are members of this group.
+    """
+    groups = Groups(topo.standalone, DEFAULT_SUFFIX, rdn=None)
+    group = groups.create(properties={'cn': 'test_group'})
+    users_list = []
+    users_num = 100
+    users = UserAccounts(topo.standalone, DEFAULT_SUFFIX, rdn=None)
+    for num in range(users_num):
+        USER_NAME = f'test_{num}'
+        user = users.create(properties={
+            'uid': USER_NAME,
+            'sn': USER_NAME,
+            'cn': USER_NAME,
+            'uidNumber': f'{num}',
+            'gidNumber': f'{num}',
+            'homeDirectory': f'/home/{USER_NAME}'
+        })
+        users_list.append(user)
+        group.add_member(user.dn)
+
+    def fin():
+        """
+        Removes group and users.
+        """
+        # If the server crashed, start it again to do the cleanup
+        if not topo.standalone.status():
+            topo.standalone.start()
+        for user in users_list:
+            user.delete()
+        group.delete()
+
+    request.addfinalizer(fin)
+
+
+@pytest.fixture(scope="function")
+def set_small_idlistscanlimit(request, topo):
+    """
+    Set nsslapd-idlistscanlimit to a smaller value to accelerate the reproducer
+    """
+    db_cfg = DatabaseConfig(topo.standalone)
+    old_idlistscanlimit = db_cfg.get_attr_vals_utf8('nsslapd-idlistscanlimit')
+    db_cfg.set([('nsslapd-idlistscanlimit', '100')])
+    topo.standalone.restart()
+
+    def fin():
+        """
+        Set nsslapd-idlistscanlimit back to the default value
+        """
+        # If the server crashed, start it again to do the cleanup
+        if not topo.standalone.status():
+            topo.standalone.start()
+        db_cfg.set([('nsslapd-idlistscanlimit', old_idlistscanlimit)])
+        topo.standalone.restart()
+
+    request.addfinalizer(fin)
 
 @pytest.mark.skipif(ds_is_older("1.4.4.4"), reason="Not implemented")
 def test_reindex_task_creates_abandoned_index_file(topo):
@@ -117,6 +178,38 @@ def test_reindex_task_creates_abandoned_index_file(topo):
     time.sleep(3)
     assert not os.path.exists(f"{inst.ds_paths.db_home_dir}/{DEFAULT_BENAME}/{attr_name.lower()}.db")
     assert os.path.exists(f"{inst.ds_paths.db_home_dir}/{DEFAULT_BENAME}/{attr_name}.db")
+
+
+@pytest.mark.bz1905450
+def test_unindexed_internal_search_crashes_server(topo, add_a_group_with_users, set_small_idlistscanlimit):
+    """
+    An internal unindexed search was able to crash the server due to missing logging function.
+
+    :id: 2d0e4070-96d6-46e5-b2c8-9495925e3e87
+    :customerscenario: True
+    :setup: Standalone instance
+    :steps:
+        1. Add a group with users
+        2. Change nsslapd-idlistscanlimit to a smaller value to accelerate the reproducer
+        3. Enable memberOf plugin
+        4. Restart the instance
+        5. Run memberOf fixup task
+        6. Wait for the task to complete
+    :expectedresults:
+        1. Should succeed
+        2. Should succeed
+        3. Should succeed
+        4. Should succeed
+        5. Should succeed
+        6. Server should not crash
+    """
+    inst = topo.standalone
+    memberof = MemberOfPlugin(inst)
+    memberof.enable()
+    inst.restart()
+    task = memberof.fixup(DEFAULT_SUFFIX)
+    task.wait()
+    assert inst.status()
 
 
 if __name__ == "__main__":
