5f48459494a2ad19488c253e03e9839c1c218687
Firstyear <william@blackhats.net.au>
2021-01-19 11:31:17 +1000

    Issue 4506 - Temporary fix for io issues (#4516)
    Issue 4506 - RFE - connection accept thread
    
    Bug Description: Previously we accepted connections and
    selected for new work in the same event loop. This could
    cause connection table polling to delay accepts, and
    accepts to delay connection activity from being ready.
    
    Fix Description: This seperates those functions allowing
    accept to occur in parallel to our normal work.
    
    fixes: https://github.com/389ds/389-ds-base/issues/4506
    
    Author: William Brown <william@blackhats.net.au>
    
    Review by: @mreynolds389 @progier389 (Thanks!)

--- ldap/servers/slapd/fe.h
+++ ldap/servers/slapd/fe.h
@@ -83,14 +83,6 @@ struct connection_table
     /* An array of connections, file descriptors, and a mapping between them. */
     Connection *c;
     struct POLL_STRUCT *fd;
-    int n_tcps; /* standard socket start index in fd */
-    int n_tcpe; /* standard socket last ( +1 ) index in fd */
-    int s_tcps; /* ssl socket start index in fd */
-    int s_tcpe; /* ssl socket last ( +1 ) in fd */
-#if defined(ENABLE_LDAPI)
-    int i_unixs; /* unix socket start index in fd */
-    int i_unixe; /* unix socket last ( +1 ) in fd */
-#endif           /* ENABLE_LDAPI */
     PRLock *table_mutex;
 };
 typedef struct connection_table Connection_Table;
