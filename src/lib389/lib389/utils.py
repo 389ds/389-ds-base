a3534437d61a0074d55ea61d02bc4a76e7448a10
Mark Reynolds <mreynolds@redhat.com>
2021-01-19 16:06:03 -0500

    Issue 4548 - CLI - dsconf needs better root DN access control plugin validation
    Description:  There is no validation done for any of the root DN access control
                  plugin settings.
    
    Relates: https://github.com/389ds/389-ds-base/issues/4548
    
    Reviewed by: spichugi & firstyear (Thanks!!)

--- src/lib389/lib389/utils.py
+++ src/lib389/lib389/utils.py
@@ -1418,3 +1418,12 @@ def cmp(self, x, y):
     and strictly positive if x > y.
     """
     return (x > y) - (x < y)
+
+
+def is_valid_hostname(hostname):
+    if len(hostname) > 255:
+        return False
+    if hostname[-1] == ".":
+        hostname = hostname[:-1] # strip exactly one dot from the right, if present
+    allowed = re.compile("(?!-)[A-Z\d-]{1,63}(?<!-)$", re.IGNORECASE)
+    return all(allowed.match(x) for x in hostname.split("."))
