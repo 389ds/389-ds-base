99e4843a7c6606394a2dd3cca137b8706bba34f3
Viktor Ashirov <vashirov@redhat.com>
2021-01-29 10:20:54 +0100

    Issue 4577 - Add GitHub actions
    Add first set of actions to compile project using gcc and clang.
    
    Relates: https://github.com/389ds/389-ds-base/issues/4577
    
    Reviewed by: @firstyear, @droideck

---
+++ .github/scripts/install_deps.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+dnf -y install 389-ds-base 389-ds-base-snmp autoconf automake \
+bind-utils bzip2 bzip2-devel cargo clang coreutils-common \
+cracklib-devel cracklib-dicts dnf-utils doxygen gcc gcc-c++ \
+git hostname icu krb5-devel libasan libcmocka-devel libdb-devel \
+libevent-devel libicu-devel libtalloc-devel libtevent-devel \
+libtool libubsan lm_sensors-devel make net-snmp-devel net-tools \
+nspr-devel nss-devel openldap-devel openssl openssl-devel pam-devel \
+pcre-devel procps-ng psmisc python3-argcomplete python3-argparse-manpage \
+python3-devel python3-ldap python3-libselinux python3-pip \
+python3-policycoreutils python3-pyasn1 python3-pyasn1-modules rpm-build \
+rsync rust systemd-devel uuid wget zlib-devel
+
