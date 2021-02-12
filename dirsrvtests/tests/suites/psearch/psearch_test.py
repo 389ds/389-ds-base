6d63bde432df4ce989d42636ef9528d6338313d0
Mark Reynolds <mreynolds@redhat.com>
2021-02-12 13:05:32 -0500

    Issue 4615 - log message when psearch first exceeds max threads per conn
    Desciption:  When a connection hits max threads per conn for the first time
                 log a message in the error.  This will help customers diagnosis
                 misbehaving clients.
    
    Fixes: https://github.com/389ds/389-ds-base/issues/4615
    
    Reviewed by: progier389(Thanks!)

--- dirsrvtests/tests/suites/psearch/psearch_test.py
+++ dirsrvtests/tests/suites/psearch/psearch_test.py
@@ -1,16 +1,17 @@
 # --- BEGIN COPYRIGHT BLOCK ---
-# Copyright (C) 2020 Red Hat, Inc.
+# Copyright (C) 2021 Red Hat, Inc.
 # All rights reserved.
 #
 # License: GPL (version 3 or any later version).
 # See LICENSE for details.
 # --- END COPYRIGHT BLOCK ---
 #
+import ldap
+import os
 import pytest
 from lib389._constants import DEFAULT_SUFFIX
 from lib389.topologies import topology_st
 from lib389.idm.group import Groups
-import ldap
 from ldap.controls.psearch import PersistentSearchControl,EntryChangeNotificationControl
 
 pytestmark = pytest.mark.tier1
