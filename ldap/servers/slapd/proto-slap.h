ada3060b9210b7ffa2fddc9ea13e3d33f35197fc
Mark Reynolds <mreynolds@redhat.com>
2021-02-12 13:05:32 -0500

    Issue 4615 - log message when psearch first exceeds max threads per conn
    Desciption:  When a connection hits max threads per conn for the first time
                 log a message in the error.  This will help customers diagnosis
                 misbehaving clients.
    
    Fixes: https://github.com/389ds/389-ds-base/issues/4615
    
    Reviewed by: progier389(Thanks!)

--- ldap/servers/slapd/proto-slap.h
+++ ldap/servers/slapd/proto-slap.h
@@ -1489,6 +1489,7 @@ int connection_release_nolock_ext(Connection *conn, int release_only);
 int connection_is_free(Connection *conn, int user_lock);
 int connection_is_active_nolock(Connection *conn);
 ber_slen_t openldap_read_function(Sockbuf_IO_Desc *sbiod, void *buf, ber_len_t len);
+int32_t connection_has_psearch(Connection *c);
 
 /*
  * saslbind.c
