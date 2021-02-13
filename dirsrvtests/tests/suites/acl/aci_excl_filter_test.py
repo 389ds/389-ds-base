c28520ed9ba8a3280d3c117fa15bbc555418c0e1
Gilbert Kimetto <gkimetto@redhat.com>
2021-02-12 21:29:04 -0500

    [IDMDS-1068] Update ticket48234_test.py and move to suites/acl/aci_excl_filter_test.py

--- dirsrvtests/tests/suites/acl/aci_excl_filter_test.py
+++ dirsrvtests/tests/suites/acl/aci_excl_filter_test.py
@@ -2,9 +2,9 @@ import pytest
 import ldap, os
 from lib389.tasks import *
 from lib389.utils import *
-from lib389.topologies import topology_st
+from lib389.topologies import topology_st as topo
 from lib389._mapped_object import DSLdapObject
-from lib389._constants import DEFAULT_SUFFIX, DN_DM, PASSWORD
+from lib389._constants import DEFAULT_SUFFIX, DN_DM, PW_DM
 
 pytestmark = pytest.mark.tier2
 
@@ -27,7 +27,7 @@ def add_user_entry(server, name, pw, myparent):
                              'userpassword': pw})))
 
 
-def test_ticket48234(topology_st):
+def test_ticket48234(topo):
     """
        Test that an ACI(Access control instruction) which contains an extensible filter.
        Test that during the schema reload task there is a small window where the new schema is not loaded
@@ -52,12 +52,14 @@ def test_ticket48234(topology_st):
         4. Operation should be successful
 
     """
-    import pdb; pdb.set_trace()
+    # import pdb; pdb.set_trace()
     log.info('Bind as root DN')
     try:
-        topology_st.standalone.simple_bind_s(DN_DM, PASSWORD)
+        ld = ldap.initialize(topo.standalone.get_ldap_uri())
+        ld.simple_bind_s(DN_DM, PW_DM)
+        
     except ldap.LDAPError as e:
-        topology_st.standalone.log.error('Root DN failed to authenticate: ' + e.args[0]['desc'])
+        topo.standalone.log.error('Root DN failed to authenticate: ' + e.args[0]['desc'])
         assert False
 
     ouname = 'outest'
@@ -71,7 +73,7 @@ def test_ticket48234(topology_st):
                 '(userdn = "ldap:///%s??sub?(&(cn=%s)(ou:dn:=%s))");)' % (DEFAULT_SUFFIX, username, ouname))
 
     try:
-        topology_st.standalone.modify_s(DEFAULT_SUFFIX, [(ldap.MOD_ADD, 'aci', ensure_bytes(aci_text))])
+        topo.standalone.modify_s(DEFAULT_SUFFIX, [(ldap.MOD_ADD, 'aci', ensure_bytes(aci_text))])
     except ldap.LDAPError as e:
         log.error('Failed to add aci: (%s) error %s' % (aci_text, e.args[0]['desc']))
         assert False
@@ -80,36 +82,37 @@ def test_ticket48234(topology_st):
     for idx in range(0, 2):
         ou0 = 'OU%d' % idx
         log.info('adding %s under %s...' % (ou0, DEFAULT_SUFFIX))
-        add_ou_entry(topology_st.standalone, ou0, DEFAULT_SUFFIX)
+        add_ou_entry(topo.standalone, ou0, DEFAULT_SUFFIX)
         parent = 'ou=%s,%s' % (ou0, DEFAULT_SUFFIX)
         log.info('adding %s under %s...' % (ouname, parent))
-        add_ou_entry(topology_st.standalone, ouname, parent)
+        add_ou_entry(topo.standalone, ouname, parent)
 
     for idx in range(0, 2):
         parent = 'ou=%s,ou=OU%d,%s' % (ouname, idx, DEFAULT_SUFFIX)
         log.info('adding %s under %s...' % (username, parent))
-        add_user_entry(topology_st.standalone, username, passwd, parent)
+        add_user_entry(topo.standalone, username, passwd, parent)
 
     binddn = 'cn=%s,%s' % (username, parent)
     log.info('Bind as user %s' % binddn)
     try:
-        topology_st.standalone.simple_bind_s(binddn, passwd)
+        # topo.standalone.simple_bind_s(binddn, passwd)
+        ld.simple_bind_s(binddn, passwd)
     except ldap.LDAPError as e:
-        topology_st.standalone.log.error(bindn + ' failed to authenticate: ' + e.args[0]['desc'])
+        topo.standalone.log.error(bindn + ' failed to authenticate: ' + e.args[0]['desc'])
         assert False
 
     cn_filter = '(cn=%s)' % username
-    print("Test username: %s" %(username ))
-    print("DEFAULT_SUFFIX:: %s ldap.SCOPE_SUBTREE:: %s cn_filter:: %s, deniedattr:: %s, dn " %(DEFAULT_SUFFIX, ldap.SCOPE_SUBTREE, cn_filter,deniedattr))
+    # print("Test username: %s" %(username ))
+    # print("DEFAULT_SUFFIX:: %s ldap.SCOPE_SUBTREE:: %s cn_filter:: %s, deniedattr:: %s, dn " %(DEFAULT_SUFFIX, ldap.SCOPE_SUBTREE, cn_filter,deniedattr))
     try:
-        entries = topology_st.standalone.search_s(DEFAULT_SUFFIX, ldap.SCOPE_SUBTREE, cn_filter, [deniedattr, 'dn'])
+        entries = topo.standalone.search_s(DEFAULT_SUFFIX, ldap.SCOPE_SUBTREE, cn_filter, [deniedattr, 'dn'])
         assert 2 == len(entries)
         for idx in range(0, 1):
             if entries[idx].hasAttr(deniedattr):
                 log.fatal('aci with extensible filter failed -- %s')
                 assert False
     except ldap.LDAPError as e:
-        topology_st.standalone.log.error('Search (%s, %s) failed: ' % (DEFAULT_SUFFIX, cn_filter) + e.args[0]['desc'])
+        topo.standalone.log.error('Search (%s, %s) failed: ' % (DEFAULT_SUFFIX, cn_filter) + e.args[0]['desc'])
         assert False
 
     log.info('Test complete')
