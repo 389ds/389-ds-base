954c0df665fec84e92f973e4fe3c24fe72150ee1
Gilbert Kimetto <gkimetto@redhat.com>
2021-03-09 18:49:48 -0500

    Issue 4654 Update ticket48234_test.py and move to suites/acl/aci_excl_filter_test.py

--- dirsrvtests/tests/suites/acl/aci_excl_filter_test.py
+++ dirsrvtests/tests/suites/acl/aci_excl_filter_test.py
@@ -1,14 +1,20 @@
+# --- BEGIN COPYRIGHT BLOCK ---
+# Copyright (C) 2021 Red Hat, Inc.
+# All rights reserved.
+#
+# License: GPL (version 3 or any later version).
+# See LICENSE for details.
+# --- END COPYRIGHT BLOCK ---
 import logging
-import pytest 
-import time, ldap, re, os
-from lib389.schema import Schema
-from lib389.utils import ensure_bytes
+import os
+import pytest
 from lib389.topologies import topology_st as topo
 from lib389._mapped_object import DSLdapObject
 from lib389.idm.organizationalunit import OrganizationalUnit
-from lib389.idm.services import ServiceAccounts
 from lib389.idm.user import UserAccounts
-from lib389._constants import DEFAULT_SUFFIX, DN_DM, PW_DM
+from lib389._constants import DEFAULT_SUFFIX
+from lib389.idm.domain import Domain
+from lib389.idm.account import Accounts
 
 pytestmark = pytest.mark.tier1
 
@@ -17,35 +23,33 @@ log = logging.getLogger(__name__)
 
 
 def add_ou_entry(topo, name, myparent):
-    
-    ou_dn = 'ou={},{}'.format(name, myparent)                     
 
-    ou = OrganizationalUnit(topo.standalone,dn=ou_dn)
-    ou.create(properties={'ou': name})
+    ou_dn = 'ou={},{}'.format(name, myparent)
+    ou = OrganizationalUnit(topo.standalone, dn=ou_dn)
+    assert ou.create(properties={'ou': name})
 
 
 def add_user_entry(topo, user, name, pw, myparent):
+
     dn = 'ou=%s,%s' % (name, myparent)
     properties = {
-            'uid': name,
-            'cn' : name,
-            'sn' : name,
-            'uidNumber': '1000',
-            'gidNumber': '2000',
-            'homeDirectory':'/home/{}'.format(name),
-            'telephonenumber': '+1 222 333-4444',
-            'userpassword' : pw,
+        'uid': name,
+        'cn': 'admin',
+        'sn': name,
+        'uidNumber': '1000',
+        'gidNumber': '2000',
+        'homeDirectory': '/home/{}'.format(name),
+        'telephonenumber': '+1 222 333-4444',
+        'userpassword': pw,
         }
 
-    user.create(properties=properties)
+    assert user.create(properties=properties)
     return user
 
 
 def test_aci_with_exclude_filter(topo):
     """
        Test an ACI(Access control instruction) which contains an extensible filter.
-       Test that during the schema reload task there is a small window where the new schema is not loaded
-       into the asi hashtables - this results in searches not returning entries.
     :id: test_aci_with_exclude_filter
     :setup: Standalone instance
     :steps:
@@ -56,7 +60,7 @@ def test_aci_with_exclude_filter(topo):
         4. Create 2 test users for each Organizational unit (ou) above with the same username 'admin'
         5. Bind to the Standalone instance as the user 'admin' from the ou created in step 4 above
            - Search for user(s) ' admin in the subtree that satisfy this criteria:
-               DEFAULT_SUFFIX, ldap.SCOPE_SUBTREE, cn_filter, [deniedattr, 'dn'] 
+               DEFAULT_SUFFIX, ldap.SCOPE_SUBTREE, cn_filter, [deniedattr, 'dn']
         6.  The search should return 2 entries with the username 'admin'
         7.  Verify that the users found do not have the --> deniedattr = 'telephonenumber' marker
     :expectedresults:
@@ -66,12 +70,10 @@ def test_aci_with_exclude_filter(topo):
         4. PASS - users found do not have the --> deniedattr = 'telephonenumber' marker
 
     """
-    log.info('Bind as root DN')
-  
-    log.info('Create an OU for them')    
+
+    log.info('Create an OU for them')
     ous = OrganizationalUnit(topo.standalone, DEFAULT_SUFFIX)
-#   # Create the OU for them
-  
+
     log.info('Create an top org users')
     users = UserAccounts(topo.standalone, DEFAULT_SUFFIX)
     log.info('Add aci which contains extensible filter.')
@@ -82,60 +84,43 @@ def test_aci_with_exclude_filter(topo):
     log.info('Add aci which contains extensible filter.')
 
     aci_text = ('(targetattr = "{}")'.format(deniedattr) +
-                 '(target = "ldap:///{}")'.format(DEFAULT_SUFFIX) +
-                 '(version 3.0;acl "admin-tel-matching-rule-outest";deny (all)' +
-                 '(userdn = "ldap:///{}??sub?(&(cn={})(ou:dn:={}))");)'.format(DEFAULT_SUFFIX, username, ouname))
-    try:
-         topo.standalone.modify_s(DEFAULT_SUFFIX, [(ldap.MOD_ADD, 'aci', ensure_bytes(aci_text))])
-    except ldap.LDAPError as e:
-         log.error('Failed to add aci: (%s) error %s' % (aci_text, e.args[0]['desc']))
-         assert False
+                '(target = "ldap:///{}")'.format(DEFAULT_SUFFIX) +
+                '(version 3.0;acl "admin-tel-matching-rule-outest";deny (all)' +
+                '(userdn = "ldap:///{}??sub?(&(cn={})(ou:dn:={}))");)'.format(DEFAULT_SUFFIX, username, ouname))
 
-    log.info('Add entries ...')
+    suffix = Domain(topo.standalone, DEFAULT_SUFFIX)
+    suffix.add('aci', aci_text)
+    log.info('Adding OU entries ...')
     for idx in range(0, 2):
         ou0 = 'OU%d' % idx
         log.info('Adding "ou" : %s under "dn" : %s...' % (ou0, DEFAULT_SUFFIX))
         add_ou_entry(topo, ou0, DEFAULT_SUFFIX)
-        
         parent = 'ou=%s,%s' % (ou0, DEFAULT_SUFFIX)
         log.info('Adding %s under %s...' % (ouname, parent))
         add_ou_entry(topo, ouname, parent)
-    
     user = UserAccounts(topo.standalone, parent, rdn=None)
     for idx in range(0, 2):
         parent = 'ou=%s,ou=OU%d,%s' % (ouname, idx, DEFAULT_SUFFIX)
+        user = UserAccounts(topo.standalone, parent, rdn=None)
         username = '{}{}'.format(username, idx)
         log.info('Adding User: %s under %s...' % (username, parent))
-        
         user = add_user_entry(topo, user, username, passwd, parent)
 
-
-    # binddn = 'cn=%s,%s' % (username, parent)
     log.info('Bind as user %s' % username)
     binddn_user = user.get(username)
-    
-    try:
-        conn = binddn_user.bind(passwd)
-        
-    except :
+
+    conn = binddn_user.bind(passwd)
+    if not conn:
         log.error(" {} failed to authenticate: ".format(binddn_user))
         assert False
 
     cn_filter = '(cn=%s)' % username
-    try:
-        # entries = topo.standalone.search_s(DEFAULT_SUFFIX, ldap.SCOPE_SUBTREE, cn_filter, [deniedattr, 'dn'])
-        entries = user.get((DEFAULT_SUFFIX, cn_filter, [deniedattr, 'dn']))
-        log.info("Entries {}".format(entries))
-        
-        assert 2 == len(entries)
-        for idx in range(0, 1):
-            if entries[idx].hasAttr(deniedattr):
-                log.fatal('aci with extensible filter failed -- %s')
-                assert False
-    except:
-        log.error('Search (%s, %s) failed: ' % (DEFAULT_SUFFIX, cn_filter))
-        assert False
-
+    entries = Accounts(conn, DEFAULT_SUFFIX).filter('(cn=admin*)')
+    log.info('Verify 2 Entries returned for cn {}'.format(cn_filter))
+    assert len(entries) == 2
+    for entry in entries:
+        assert not entry.get_attr_val_utf8('telephonenumber')
+        log.info("Verified the entries do not contain 'telephonenumber' ")
     log.info('Test complete')
 
 
