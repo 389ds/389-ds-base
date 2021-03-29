73351b9ba6829614e2bfd4515d0ac12600b79408
Mark Reynolds <mreynolds@redhat.com>
2021-03-29 15:44:10 -0400

    Issue 2736 - https://github.com/389ds/389-ds-base/issues/2736
    Description:  Adjust perl and python scripts shebangs for be absolute values
    
    relates: https://github.com/389ds/389-ds-base/issues/2736
    
    Reviewed by: firstyear(Thanks!)

--- ldap/admin/src/scripts/failedbinds.py
+++ ldap/admin/src/scripts/failedbinds.py
@@ -1,3 +1,13 @@
+#!/usr/bin/python3
+
+# --- BEGIN COPYRIGHT BLOCK ---
+# Copyright (C) 2021 Red Hat, Inc.
+# All rights reserved.
+#
+# License: GPL (version 3 or any later version).
+# See LICENSE for details.
+# --- END COPYRIGHT BLOCK ---
+
 import re
 import sys
 import os, os.path
