a08540a828dde2f2e1a8d157c38a9be85a5d2876
James Chapman <jachapma@redhat.com>
2021-05-04 15:48:10 +0100

    Issue 4750 - Fix compiler warning in retrocl (#4751)
    Description: An unused variable generates a compiler warning.
    
    Fix description: Remove unused variable. Modify CI test to restart the test instance instead
    		         of using dynamic plugins.
    
    Fixes: https://github.com/389ds/389-ds-base/issues/4750
    
    Relates: https://github.com/389ds/389-ds-base/issues/4701
    
    Reviewed by: jchapma (One line commit rule)

--- dirsrvtests/tests/suites/retrocl/basic_test.py
+++ dirsrvtests/tests/suites/retrocl/basic_test.py
@@ -60,13 +60,6 @@ def test_retrocl_exclude_attr_add(topology_st):
 
     st = topology_st.standalone
 
-    log.info('Enable dynamic plugins')
-    try:
-        st.config.set('nsslapd-dynamic-plugins', 'on')
-    except ldap.LDAPError as e:
-        ldap.error('Failed to enable dynamic plugins ' + e.args[0]['desc'])
-        assert False
-
     log.info('Configure retrocl plugin')
     rcl = RetroChangelogPlugin(st)
     rcl.disable()
@@ -131,8 +124,12 @@ def test_retrocl_exclude_attr_add(topology_st):
     disconnect_instance(inst)
     assert result is None
 
-    log.info("5s delay for retrocl plugin to restart")
-    time.sleep(5)
+    log.info('Restarting instance')
+    try:
+        st.restart()
+    except ldap.LDAPError as e:
+        ldap.error('Failed to restart instance ' + e.args[0]['desc'])
+        assert False
 
     log.info('Adding user2')
     try:
@@ -194,13 +191,6 @@ def test_retrocl_exclude_attr_mod(topology_st):
     """
 
     st = topology_st.standalone
-
-    log.info('Enable dynamic plugins')
-    try:
-        st.config.set('nsslapd-dynamic-plugins', 'on')
-    except ldap.LDAPError as e:
-        ldap.error('Failed to enable dynamic plugins ' + e.args[0]['desc'])
-        assert False
 
     log.info('Configure retrocl plugin')
     rcl = RetroChangelogPlugin(st)
@@ -266,8 +256,12 @@ def test_retrocl_exclude_attr_mod(topology_st):
     disconnect_instance(inst)
     assert result is None
 
-    log.info("5s delay for retrocl plugin to restart")
-    time.sleep(5)
+    log.info('Restarting instance')
+    try:
+        st.restart()
+    except ldap.LDAPError as e:
+        ldap.error('Failed to restart instance ' + e.args[0]['desc'])
+        assert False
 
     log.info('Modify user1 carLicense attribute')
     try:
