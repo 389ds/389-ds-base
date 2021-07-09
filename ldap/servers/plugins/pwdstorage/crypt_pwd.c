c1926dfc6591b55c4d33f9944de4d7ebe077e964
Firstyear <william@blackhats.net.au>
2021-07-09 11:53:35 +1000

    Issue 4817 - BUG - locked crypt accounts on import may allow all passwords (#4819)
    Bug Description: Due to mishanding of short dbpwd hashes, the
    crypt_r algorithm was misused and was only comparing salts
    in some cases, rather than checking the actual content
    of the password.
    
    Fix Description: Stricter checks on dbpwd lengths to ensure
    that content passed to crypt_r has at least 2 salt bytes and
    1 hash byte, as well as stricter checks on ct_memcmp to ensure
    that compared values are the same length, rather than potentially
    allowing overruns/short comparisons.
    
    fixes: https://github.com/389ds/389-ds-base/issues/4817
    
    Author: William Brown <william@blackhats.net.au>
    
    Review by: @mreynolds389

--- ldap/servers/plugins/pwdstorage/crypt_pwd.c
+++ ldap/servers/plugins/pwdstorage/crypt_pwd.c
@@ -48,15 +48,23 @@ static unsigned char itoa64[] = /* 0 ... 63 => ascii - 64 */
 int
 crypt_pw_cmp(const char *userpwd, const char *dbpwd)
 {
-    int rc;
-    char *cp;
+    int rc = -1;
+    char *cp = NULL;
+    size_t dbpwd_len = strlen(dbpwd);
     struct crypt_data data;
     data.initialized = 0;
 
-    /* we use salt (first 2 chars) of encoded password in call to crypt_r() */
-    cp = crypt_r(userpwd, dbpwd, &data);
-    if (cp) {
-        rc = slapi_ct_memcmp(dbpwd, cp, strlen(dbpwd));
+    /*
+     * there MUST be at least 2 chars of salt and some pw bytes, else this is INVALID and will
+     * allow any password to bind as we then only compare SALTS.
+     */
+    if (dbpwd_len >= 3) {
+        /* we use salt (first 2 chars) of encoded password in call to crypt_r() */
+        cp = crypt_r(userpwd, dbpwd, &data);
+    }
+    /* If these are not the same length, we can not proceed safely with memcmp. */
+    if (cp && dbpwd_len == strlen(cp)) {
+        rc = slapi_ct_memcmp(dbpwd, cp, dbpwd_len);
     } else {
         rc = -1;
     }
