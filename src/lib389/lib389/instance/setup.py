18ee387c72ac1f95e694104a8e2df69a78601579
Mark Reynolds <mreynolds@redhat.com>
2021-11-01 10:42:27 -0400

    Issue 4973 - installer changes permissions on /run
    Description:  There was a regression when we switched over to using /run
                  that caused the installer to try and create /run which
                  caused the ownership to change.  Fixed this by changing
                  the "run_dir" to /run/dirsrv
    
    relates: https://github.com/389ds/389-ds-base/issues/4973
    
    Reviewed by: jchapman(Thanks!)

--- src/lib389/lib389/instance/setup.py
+++ src/lib389/lib389/instance/setup.py
@@ -732,10 +732,6 @@ class SetupDs(object):
                 dse += line.replace('%', '{', 1).replace('%', '}', 1)
 
         with open(os.path.join(slapd['config_dir'], 'dse.ldif'), 'w') as file_dse:
-            if os.path.exists(os.path.dirname(slapd['ldapi'])):
-                ldapi_path = slapd['ldapi']
-            else:
-                ldapi_path = os.path.join(slapd['run_dir'], "slapd-%s.socket" % slapd['instance_name'])
             dse_fmt = dse.format(
                 schema_dir=slapd['schema_dir'],
                 lock_dir=slapd['lock_dir'],
@@ -759,7 +755,7 @@ class SetupDs(object):
                 db_dir=slapd['db_dir'],
                 db_home_dir=slapd['db_home_dir'],
                 ldapi_enabled="on",
-                ldapi=ldapi_path,
+                ldapi=slapd['ldapi'],
                 ldapi_autobind="on",
             )
             file_dse.write(dse_fmt)
@@ -861,7 +857,7 @@ class SetupDs(object):
             SER_ROOT_PW: self._raw_secure_password,
             SER_DEPLOYED_DIR: slapd['prefix'],
             SER_LDAPI_ENABLED: 'on',
-            SER_LDAPI_SOCKET: ldapi_path,
+            SER_LDAPI_SOCKET: slapd['ldapi'],
             SER_LDAPI_AUTOBIND: 'on'
         }
 
@@ -905,12 +901,9 @@ class SetupDs(object):
             self.log.info("Perform SELinux labeling ...")
             selinux_paths = ('backup_dir', 'cert_dir', 'config_dir', 'db_dir',
                              'ldif_dir', 'lock_dir', 'log_dir', 'db_home_dir',
-                             'schema_dir', 'tmp_dir')
+                             'run_dir', 'schema_dir', 'tmp_dir')
             for path in selinux_paths:
                 selinux_restorecon(slapd[path])
-
-            # Don't run restorecon on the entire /run directory
-            selinux_restorecon(slapd['run_dir'] + '/dirsrv')
 
             selinux_label_port(slapd['port'])
 
