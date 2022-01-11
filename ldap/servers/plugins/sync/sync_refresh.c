a2a55e8b0efec8df59b83e5456d13f9637ba9bf2
tbordaz <tbordaz@redhat.com>
2022-01-11 15:44:27 +0100

    Issue 5095 - sync-repl with openldap may send truncated syncUUID (#5099)
    Bug description:
    	When using sync_repl from openldap, syncUUID (that identify an
    	entry) is retrieved from targetEntryUUID rather than nsuniqueid.
    	syncUUID is a 16 bytes long representation of targetEntryUUID.
            TargetEntryUUID can contain '00' so syncUUID can contain a
    	byte with 0x00.
    	When creating a syncInfo
    (https://datatracker.ietf.org/doc/html/rfc4533#section-2.5)
    	syncUUIDS is ber encoded with '[v]' taking a null terminated
    	array of (char*). In such case the 0x00 char truncates the
    	syncUUID.
    
    Fix description:
    	Instead of using a null terminated array of (char*), the
    	fix uses a null terminated array of berval.
    
    relates: https://github.com/389ds/389-ds-base/issues/5095
    
    Reviewed by: William Brown, Simon Pichugin (Thanks)
    
    Platforms tested: F34

--- ldap/servers/plugins/sync/sync_refresh.c
+++ ldap/servers/plugins/sync/sync_refresh.c
@@ -745,6 +745,7 @@ void
 sync_send_deleted_entries(Slapi_PBlock *pb, Sync_UpdateNode *upd, int chg_count, Sync_Cookie *cookie)
 {
     char *syncUUIDs[SYNC_MAX_DELETED_UUID_BATCH + 1] = {0};
+    struct berval *ber_syncUUIDs[SYNC_MAX_DELETED_UUID_BATCH + 1] = {0};
     size_t uuid_index = 0;
 
     PR_ASSERT(cookie);
@@ -764,9 +765,15 @@ sync_send_deleted_entries(Slapi_PBlock *pb, Sync_UpdateNode *upd, int chg_count,
             } else {
                 /* max number of uuids to be sent in one sync info message */
                 syncUUIDs[uuid_index] = NULL;
-                sync_intermediate_msg(pb, LDAP_TAG_SYNC_ID_SET, cookie, (char **)&syncUUIDs);
+                for (size_t i = 0; i < uuid_index; i++) {
+                    ber_syncUUIDs[i] = (struct berval *) slapi_ch_malloc(sizeof(struct berval));
+                    ber_syncUUIDs[i]->bv_val = syncUUIDs[i];
+                    ber_syncUUIDs[i]->bv_len = 16;
+                }
+                sync_intermediate_msg(pb, LDAP_TAG_SYNC_ID_SET, cookie, ber_syncUUIDs);
                 for (size_t i = 0; i < uuid_index; i++) {
                     slapi_ch_free((void **)&syncUUIDs[i]);
+                    slapi_ch_free((void **)&ber_syncUUIDs[i]);
                     syncUUIDs[i] = NULL;
                 }
                 uuid_index = 0;
@@ -777,9 +784,15 @@ sync_send_deleted_entries(Slapi_PBlock *pb, Sync_UpdateNode *upd, int chg_count,
     if (uuid_index > 0 && syncUUIDs[uuid_index - 1]) {
         /* more entries to send */
         syncUUIDs[uuid_index] = NULL;
-        sync_intermediate_msg(pb, LDAP_TAG_SYNC_ID_SET, cookie, (char **)&syncUUIDs);
+        for (size_t i = 0; i < uuid_index; i++) {
+            ber_syncUUIDs[i] = (struct berval *) slapi_ch_malloc(sizeof(struct berval));
+            ber_syncUUIDs[i]->bv_val = syncUUIDs[i];
+            ber_syncUUIDs[i]->bv_len = 16;
+        }
+        sync_intermediate_msg(pb, LDAP_TAG_SYNC_ID_SET, cookie, ber_syncUUIDs);
         for (size_t i = 0; i < uuid_index; i++) {
             slapi_ch_free((void **)&syncUUIDs[i]);
+            slapi_ch_free((void **)&ber_syncUUIDs[i]);
             syncUUIDs[i] = NULL;
         }
     }
