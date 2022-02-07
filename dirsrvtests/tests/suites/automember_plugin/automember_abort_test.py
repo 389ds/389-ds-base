a10c86072cb7827f749da4e72702893ac83e54fa
Mark Reynolds <mreynolds@redhat.com>
2022-02-07 18:16:02 -0500

    Issue 5155 - RFE - Provide an option to abort an Auto Member rebuild task
    Description:  Add an abort task for the automember rebuild task.  There
    are cases where IPA can start spinning up schema compat search during
    the rebuild which can bog down an entire system.  If this happens the
    task can be aborted to prevent an outage.
    
    The transaction for the entire task was also removed since it isn't
    really needed for a fixup task.
    
    Also found that in cleanAllRUV we wre trying to write to the task entry
    in the task add callback function (which is too early to start updating
    the task and triggers error 32 messages in the errors log).  So that
    was fixed as well.
    
    relates: https://github.com/389ds/389-ds-base/issues/5155
    
    Reviewed by: progier, tbordaz, spichugi (Thanks!!!)

---
+++ dirsrvtests/tests/suites/automember_plugin/automember_abort_test.py
@@ -0,0 +1,94 @@
+import logging
+import pytest
+import os
+import time
+from lib389._constants import DEFAULT_SUFFIX
+from lib389.plugins import AutoMembershipPlugin, AutoMembershipDefinitions
+from lib389.idm.user import UserAccounts
+from lib389.idm.group import Groups
+from lib389.topologies import topology_st as topo
+
+log = logging.getLogger(__name__)
+
+
+@pytest.fixture(scope="module")
+def automember_fixture(topo, request):
+    # Create group
+    group_obj = Groups(topo.standalone, DEFAULT_SUFFIX)
+    automem_group = group_obj.create(properties={'cn': 'testgroup'})
+
+    # Create users
+    users = UserAccounts(topo.standalone, DEFAULT_SUFFIX, rdn=None)
+    NUM_USERS = 1000
+    for num in range(NUM_USERS):
+        num_ran = int(round(num))
+        USER_NAME = 'test%05d' % num_ran
+        users.create(properties={
+            'uid': USER_NAME,
+            'sn': USER_NAME,
+            'cn': USER_NAME,
+            'uidNumber': '%s' % num_ran,
+            'gidNumber': '%s' % num_ran,
+            'homeDirectory': '/home/%s' % USER_NAME,
+            'mail': '%s@redhat.com' % USER_NAME,
+            'userpassword': 'pass%s' % num_ran,
+        })
+
+
+    # Create automember definitions and regex rules
+    automember_prop = {
+        'cn': 'testgroup_definition',
+        'autoMemberScope': DEFAULT_SUFFIX,
+        'autoMemberFilter': 'objectclass=posixaccount',
+        'autoMemberDefaultGroup': automem_group.dn,
+        'autoMemberGroupingAttr': 'member:dn',
+    }
+    automembers = AutoMembershipDefinitions(topo.standalone)
+    auto_def = automembers.create(properties=automember_prop)
+    auto_def.add_regex_rule("regex1", automem_group.dn, include_regex=['uid=.*'])
+
+    # Enable plugin
+    automemberplugin = AutoMembershipPlugin(topo.standalone)
+    automemberplugin.enable()
+    topo.standalone.restart()
+
+
+def test_abort(automember_fixture, topo):
+    """Test the abort rebuild task
+
+    :id: 24763279-48ec-4c34-91b3-f681679dec3a
+    :setup: Standalone Instance
+    :steps:
+        1. Setup automember and create a bunch of users
+        2. Start rebuild task
+        3. Abort rebuild task
+        4. Verify rebuild task was aborted
+    :expectedresults:
+        1. Success
+        2. Success
+        3. Success
+        4. Success
+    """
+
+    automemberplugin = AutoMembershipPlugin(topo.standalone)
+
+    # Run rebuild task
+    task = automemberplugin.fixup(DEFAULT_SUFFIX, "objectclass=top")
+    time.sleep(1)
+
+    # Abort rebuild task
+    automemberplugin.abort_fixup()
+
+    # Wait for rebuild task to finish
+    task.wait()
+
+    # Check errors log for abort message
+    assert topo.standalone.searchErrorsLog("task was intentionally aborted")
+
+
+if __name__ == '__main__':
+    # Run isolated
+    # -s for DEBUG mode
+    CURRENT_FILE = os.path.realpath(__file__)
+    pytest.main(["-s", CURRENT_FILE])
+
