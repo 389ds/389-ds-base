cc255cec860c094710cfd254ac262ec615fdc3a0
Pierre Rogier <progier@redhat.com>
2022-03-22 14:45:00 +0100

    Issue 5217 - Simplify instance creation and administration by non root user

--- src/lib389/lib389/cli_ctl/instance.py
+++ src/lib389/lib389/cli_ctl/instance.py
@@ -7,8 +7,11 @@
 # --- END COPYRIGHT BLOCK ---
 
 import os
+import pwd
+import grp
 import json
 import time
+from shutil import copytree, copyfile
 from lib389 import DirSrv
 from lib389.instance.setup import SetupDs
 from lib389.utils import get_instance_list
@@ -117,6 +120,135 @@ def instance_example(inst, log, args):
         print(g2b.collect_help(advanced=args.advanced))
         print(s2b.collect_help(advanced=args.advanced))
         print(b2b.collect_help(advanced=args.advanced))
+    return True
+
+
+def prepare_ds_root(inst, log, args):
+    def get_dest(path):
+        # Compute the destination path from original path
+        if path.startswith('/usr/'):
+            return f'{args.root_dir}/{path[5:]}'
+        return f'{args.root_dir}/{path}'
+
+    def copy_and_substitute(file, sub_list):
+        # Copy .inf file and apply all sub_list modifiers to each line
+        #  Supported modifier are:
+        #    (WORD, pattern, value) ==> Replace first occurance of pattern by value
+        #    (LINE, pattern, value) ==> Replace line statring with pattern
+        #                                 by: pattern = value
+        log.debug(f'Update {get_dest(file)} from {file}')
+        with open(file, 'rt') as fin:
+            with open(get_dest(file), 'wt') as fout:
+                for line in fin:
+                    for k, p, v in sub_list:
+                        if k == 'WORD':
+                            line = line.replace(p, v, 1)
+                        if k == 'LINE':
+                            if line.startswith(p):
+                                line = f'{p} = {v}\n'
+                    fout.write(line)
+
+    def copy_and_skip_entry(file, pattern_list):
+        # copy ldif file skipping entries containing a pattern from pattern_list
+        class Entry:
+            # Helper class to handle the ldif entries
+            def __init__(self):
+                self.skip = False
+                self.entry = ""
+
+            def add_line(self, line):
+                self.entry += line
+                for p in pattern_list:
+                    if p in line:
+                        self.skip = True
+
+            def write(self, fout):
+                if not self.skip:
+                    fout.write(self.entry)
+
+        log.debug(f'Update {get_dest(file)} from {file}')
+        with open(file, 'rt') as fin:
+            with open(get_dest(file), 'wt') as fout:
+                entry = Entry()
+                for line in fin:
+                    entry.add_line(line)
+                    if line == "\n":
+                        entry.write(fout)
+                        entry = Entry()
+                entry.write(fout)
+
+    uid = os.getuid()
+    if uid == 0:
+        raise ValueError("ds-root subcommand should not be run by root user.")
+    user = pwd.getpwuid(uid).pw_name
+    group = grp.getgrgid(os.getgid()).gr_name
+
+    # Perform consistency checks then create the wrappers
+    if args.bin_dir:
+        found = False
+        for path in os.environ['PATH'].split(':'):
+            if path.startswith('.'):
+                continue
+            if path == args.bin_dir:
+                found = True
+                break
+            if os.path.exists(f'{path}/dsconf'):
+                log.error(f'bin_dir argument should be before {path} in PATH')
+                return False
+        if not found:
+            log.error(f'bin_dir argument should be in PATH')
+            return False
+        os.makedirs(args.bin_dir, 0o755, True)
+        for wrapper in ['dsconf',  'dscreate',  'dsctl',  'dsidm']:
+            log.debug(f'Creating {args.bin_dir}/{wrapper} wrapper')
+            with open(f'{args.bin_dir}/{wrapper}', 'wt') as f:
+                f.write('#!/bin/sh\n')
+                f.write(f'export PATH="{args.root_dir}:$PATH"\n')
+                f.write(f'export PREFIX="{args.root_dir}"\n')
+                f.write(f'export INSTALL_PREFIX="{args.root_dir}"\n')
+                p = "${@}"
+                f.write(f'exec /usr/sbin/{wrapper} "{p}"\n')
+            os.chmod(f'{args.bin_dir}/{wrapper}', 0o755)
+    os.makedirs(args.root_dir, 0o700, True)
+    # Copy subtrees
+    for dir in ['/usr/share/dirsrv/', '/etc/dirsrv/config',  '/etc/dirsrv/schema', ]:
+        destdir = get_dest(dir)
+        log.debug(f'Copying {dir} into {destdir}')
+        os.makedirs(destdir, 0o755, True)
+        copytree(dir, destdir, dirs_exist_ok=True)
+    # Create empty directories
+    for dir in ['/tmp', ]:
+        destdir = get_dest(dir)
+        log.debug(f'Creating directory {destdir}')
+        os.makedirs(destdir, 0o755, True)
+    # Copy binaries
+    for bin in ['/usr/sbin/ns-slapd']:
+        destbin = get_dest(bin)
+        log.debug(f'Copying {bin} into {destbin}')
+        os.makedirs(os.path.dirname(destbin), 0o755, True)
+        copyfile(bin, destbin)
+    # And finally, update the template files
+    for dse in ('/usr/share/dirsrv/data/template-dse-minimal.ldif', '/usr/share/dirsrv/data/template-dse.ldif'):
+        copy_and_skip_entry(dse, ('libpwdchan-plugin',))
+    # Use PO and PF to escape { } in formatted strings
+    PO = '{'
+    PF = '}'
+    copy_and_substitute('/usr/share/dirsrv/inf/defaults.inf', (
+            ('WORD', ' /', f' {args.root_dir}/'),
+            ('LINE', 'with_selinux', 'no'),
+            ('LINE', 'with_systemd', '0'),
+            ('LINE', 'user', user),
+            ('LINE', 'group', group),
+            ('LINE', 'prefix', args.root_dir),
+            ('LINE', 'bin_dir', '/usr/bin'),
+            ('LINE', 'sbin_dir', '/usr/sbin'),
+            ('LINE', 'lib_dir', '/usr/lib64'),
+            ('LINE', 'data_dir', f'{args.root_dir}/share'),
+            ('LINE', 'inst_dir', f'{args.root_dir}/lib64/slapd-{PO}instance_name{PF}'),
+            ('LINE', 'plugin_dir', '/usr/lib64/dirsrv/plugins'),
+            ('LINE', 'system_schema_dir', f'{args.root_dir}/share/dirsrv/schema'),
+       ))
+
     return True
 
 
