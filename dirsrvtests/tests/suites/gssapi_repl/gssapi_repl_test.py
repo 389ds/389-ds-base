ab8e0e7caa0962a2b12615bf09052d3c3711031c
progier389 <progier@redhat.com>
2022-05-02 18:43:25 +0200

    Issue 5126 - Memory leak in slapi_ldap_get_lderrno (#5153)
    * Issue 5126 - Memory leak in slapi_ldap_get_lderrno
    
    The problem is that some time ago libldap API replaced  ​LDAP_OPT_ERROR_STRING whose data should not be freed by
    LDAP_OPT_DIAGNOSTIC_MESSAGE whose data must be freed.
    slapi_ldap_get_lderrno was adapted to use the new option but the callers were not modified to free the value.
    
    The Solution:
     Insure that we also need to free slapi_ldap_get_lderrno value if legacy LDAP_OPT_ERROR_STRING is used (by duping the value)
     Insure that the callers free the value.
    
    Added test case about replication using SASL/Digest-md5 authentication
    Added test case to check this leak
    Also updated test case about SASL/GSSAPI to be comapatible with current lib389 framework but marked as skipped because it requires a specific configuration (This path should be tested by IPA tests)
    Fixed valgrind lib389 function to run on prefixed installation without needing to be root.
    At last I also improved lib389 mapped object to have a better diagnostic when LDAP operation fails (by adding the request within the exception)
    
     issue: 5126 https://github.com/389ds/389-ds-base/issues/5126
    
    Reviewd by: @droideck
    
    (cherry picked from commit 4d89e11494233d8297896540bc752cfdbab2cc69)

--- dirsrvtests/tests/suites/gssapi_repl/gssapi_repl_test.py
+++ dirsrvtests/tests/suites/gssapi_repl/gssapi_repl_test.py
@@ -9,6 +9,7 @@
 import pytest
 from lib389.tasks import *
 from lib389.utils import *
+from lib389.agreement import *
 from lib389.topologies import topology_m2
 
 pytestmark = pytest.mark.tier2
@@ -65,10 +66,27 @@ def _allow_machine_account(inst, name):
     # First we need to get the mapping tree dn
     mt = inst.mappingtree.list(suffix=DEFAULT_SUFFIX)[0]
     inst.modify_s('cn=replica,%s' % mt.dn, [
-        (ldap.MOD_REPLACE, 'nsDS5ReplicaBindDN', "uid=%s,ou=Machines,%s" % (name, DEFAULT_SUFFIX))
+        (ldap.MOD_REPLACE, 'nsDS5ReplicaBindDN', f"uid={name},ou=Machines,{DEFAULT_SUFFIX}".encode('utf-8'))
     ])
 
+def _verify_etc_hosts():
+    #Check if /etc/hosts is compatible with the test
+    NEEDED_HOSTS = ( ('ldapkdc.example.com', '127.0.0.1'),
+                     ('ldapkdc1.example.com', '127.0.1.1'),
+                     ('ldapkdc2.example.com', '127.0.2.1'))
+    found_hosts = {}
+    with open('/etc/hosts','r') as f:
+        for l in f:
+            s = l.split()
+            if len(s) < 2:
+                continue
+            for nh in NEEDED_HOSTS:
+                if (s[0] == nh[1] and s[1] == nh[0]):
+                    found_hosts[s[1]] = True
+    return len(found_hosts) == len(NEEDED_HOSTS)
 
+@pytest.mark.skipif(not _verify_etc_hosts(), reason="/etc/hosts does not contains the needed hosts.")
+@pytest.mark.skipif(True, reason="Test disabled because it requires specific kerberos requirement (server principal, keytab, etc ...")
 def test_gssapi_repl(topology_m2):
     """Test gssapi authenticated replication agreement of two suppliers using KDC
 
@@ -94,8 +112,6 @@ def test_gssapi_repl(topology_m2):
          6. Test User should be created on M1 and M2 both
          7. Test User should be created on M1 and M2 both
     """
-
-    return
     supplier1 = topology_m2.ms["supplier1"]
     supplier2 = topology_m2.ms["supplier2"]
 
@@ -121,6 +137,7 @@ def test_gssapi_repl(topology_m2):
     properties = {RA_NAME: r'meTo_$host:$port',
                   RA_METHOD: 'SASL/GSSAPI',
                   RA_TRANSPORT_PROT: defaultProperties[REPLICATION_TRANSPORT]}
+    supplier1.agreement.delete(suffix=SUFFIX, consumer_host=supplier2.host, consumer_port=supplier2.port)
     m1_m2_agmt = supplier1.agreement.create(suffix=SUFFIX, host=supplier2.host, port=supplier2.port, properties=properties)
     if not m1_m2_agmt:
         log.fatal("Fail to create a supplier -> supplier replica agreement")
@@ -133,6 +150,7 @@ def test_gssapi_repl(topology_m2):
     properties = {RA_NAME: r'meTo_$host:$port',
                   RA_METHOD: 'SASL/GSSAPI',
                   RA_TRANSPORT_PROT: defaultProperties[REPLICATION_TRANSPORT]}
+    supplier2.agreement.delete(suffix=SUFFIX, consumer_host=supplier1.host, consumer_port=supplier1.port)
     m2_m1_agmt = supplier2.agreement.create(suffix=SUFFIX, host=supplier1.host, port=supplier1.port, properties=properties)
     if not m2_m1_agmt:
         log.fatal("Fail to create a supplier -> supplier replica agreement")
@@ -145,8 +163,9 @@ def test_gssapi_repl(topology_m2):
     #
     # Initialize all the agreements
     #
-    supplier1.agreement.init(SUFFIX, HOST_SUPPLIER_2, PORT_SUPPLIER_2)
-    supplier1.waitForReplInit(m1_m2_agmt)
+    agmt = Agreement(supplier1, m1_m2_agmt)
+    agmt.begin_reinit()
+    agmt.wait_reinit()
 
     # Check replication is working...
     if supplier1.testReplication(DEFAULT_SUFFIX, supplier2):
