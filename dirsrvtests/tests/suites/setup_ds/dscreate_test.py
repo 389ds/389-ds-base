478f1e378943224fa0cfcc7fce350f24b44d513d
progier389 <progier@redhat.com>
2022-09-12 18:21:56 +0200

    Issue 4592: dscreate error with custom dir_path (#5434)
    * Issue 4592: dscreate error with custom dir_path
    
        Problem:
        When creating an instance with a custom dir_path and if seLinux is enabled then the instance creation fails:
        Instance fails to start because database cannot be created due to seLinux permission error.
    
        Solution:
        The solution is to set the right seLinux label for the instance directories by deleting current context
        and adding new one
        but there are some pitfalls to avoid:
           - Make sure that the right path is used (otherwise semanage fails)
           - Make sure that a path is not associated with different labels
             ( for example by setting the same path in db_dir and in tmp_dir )
        The idea is to parse the semanage fcontext -l [-C] output to get the:
            path equivalence rules (about symbolic link)
            local file context path and labels
            global policy file context path and labels
        And use them to compute the right path, and to decide what must be done
          (nothing, delete existing local context, raise an error or add new context)
    
        Note: also fixed some lib389 warning about obsolete escape sequences
    
        Issue: 4592
    
        Reviewed by: @droideck
    
    (cherry picked from commit 199ec65576b4d27351327e9eb02ebcce5e7a36f7)

--- dirsrvtests/tests/suites/setup_ds/dscreate_test.py
+++ dirsrvtests/tests/suites/setup_ds/dscreate_test.py
@@ -9,6 +9,7 @@ import sys
 import pytest
 import subprocess
 import logging
+import grp
 import pwd
 import re
 from tempfile import TemporaryDirectory
@@ -18,7 +19,8 @@ from lib389.instance.setup import SetupDs
 from lib389.instance.remove import remove_ds_instance
 from lib389.instance.options import General2Base, Slapd2Base
 from lib389._constants import *
-from lib389.utils import ds_is_older
+from lib389.utils import ds_is_older, selinux_label_file, ensure_list_str, ensure_str
+from shutil import rmtree
 
 pytestmark = [pytest.mark.tier0,
               pytest.mark.skipif(ds_is_older('1.4.1.2'), reason="Needs a compatible systemd unit, see PR#50213")]
@@ -37,6 +39,9 @@ INSTANCE_SERVERID = 'standalone'
 
 MAJOR, MINOR, _, _, _ = sys.version_info
 
+CUSTOM_DIR = f'{os.getenv("PREFIX", "")}/var/lib/dirsrv_pytest_test_setup_ds_custom_db_dir'
+CUSTOM_DB_DIR = f'{CUSTOM_DIR}/db'
+
 class TopologyInstance(object):
     def __init__(self, standalone):
         # For these tests, we don't want to open the instance.
@@ -53,13 +58,29 @@ def topology(request):
     instance.allocate(args)
     if instance.exists():
         instance.delete()
+    # Cleanup custom dir
+    selinux_label_file(CUSTOM_DB_DIR, None)
+    rmtree(CUSTOM_DIR, ignore_errors=True)
 
     def fin():
-        if instance.exists() and not DEBUGGING:
-            instance.delete()
+        if not DEBUGGING:
+            if instance.exists():
+                instance.delete()
+            selinux_label_file(CUSTOM_DB_DIR, None)
+            rmtree(CUSTOM_DIR, ignore_errors=True)
     request.addfinalizer(fin)
 
     return TopologyInstance(instance)
+
+
+def run_cmd(cmd):
+    result = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    args = ' '.join(ensure_list_str(result.args))
+    stdout = ensure_str(result.stdout)
+    stderr = ensure_str(result.stderr)
+    log.info(f"CMD: {args} returned {result.returncode} STDOUT: {stdout} STDERR: {stderr}")
+    return stdout
+
 
 def test_setup_ds_minimal_dry(topology):
     # Unset PYTHONPATH to avoid mixing old CLI tools and new lib389
@@ -128,6 +149,92 @@ def test_setup_ds_minimal(topology):
     # Okay, actually remove the instance
     remove_ds_instance(topology.standalone)
 
+
+@pytest.mark.skipif(not os.path.exists('/usr/sbin/semanage'), reason="semanage is not installed. Please run dnf install policycoreutils-python-utils -y")
+@pytest.mark.skipif(os.getuid()!=0, reason="pytest non run by root user")
+def test_setup_ds_custom_db_dir(topology):
+    """Test DS setup using custom uid,gid and db_dir path
+
+    :id: 5a596887-cabb-4862-a91c-5eedafe222cd
+    :setup: standalone instance
+    :steps:
+        1. Create the user that will run ns-slapd
+        2. Create the setupDS
+        3. Give it the right types
+        4. Get the dicts from Type2Base, as though they were from _validate_ds_2_config
+        5. Override instance name, root password, port, secure port, user, group and dir_path
+        6. Assert we did change the system
+        7. Make sure we can connect
+        8. Make sure we can start stop.
+        9. Remove the instance
+        10. Check that there is not any dirsrv_* labels in in file local selinux customizations
+        11. Check that there is not any wldap_port_t labels in port local selinux customizations
+    :expectedresults:
+        1. Success
+        2. Success
+        3. Success
+        4. Success
+        5. Success
+        6. Success
+        7. Success
+        8. Success
+        9. Success
+        10. Success
+        11. Success
+    """
+  # Add linux user NON_ROOT_USER if it does not already exist
+    CUSTOM_USER='ldapsrv1'
+    try:
+        pwd_cu = pwd.getpwnam(CUSTOM_USER)
+    except KeyError:
+        subprocess.run(('/usr/sbin/useradd', CUSTOM_USER), check=True)
+        pwd_cu = pwd.getpwnam(CUSTOM_USER)
+    grp_cu = grp.getgrgid(pwd_cu.pw_gid)
+    log.info(f'Custom user: {pwd_cu} {grp_cu}')
+
+    # Create the setupDs
+    lc = LogCapture()
+    # Give it the right types.
+    sds = SetupDs(verbose=DEBUGGING, dryrun=False, log=lc.log)
+
+    # Get the dicts from Type2Base, as though they were from _validate_ds_2_config
+    # IE get the defaults back just from Slapd2Base.collect
+    # Override instance name, root password, port, secure port, user,  group and db_dir.
+
+    general_options = General2Base(lc.log)
+    general_options.verify()
+    general = general_options.collect()
+
+    slapd_options = Slapd2Base(lc.log)
+    slapd_options.set('instance_name', INSTANCE_SERVERID)
+    slapd_options.set('port', INSTANCE_PORT)
+    slapd_options.set('secure_port', INSTANCE_SECURE_PORT)
+    slapd_options.set('root_password', PW_DM)
+    slapd_options.set('user', pwd_cu.pw_name)
+    slapd_options.set('group', grp_cu.gr_name)
+    slapd_options.set('db_dir', CUSTOM_DB_DIR)
+    slapd_options.verify()
+    slapd = slapd_options.collect()
+
+    sds.create_from_args(general, slapd, {}, None)
+    insts = topology.standalone.list(serverid=INSTANCE_SERVERID)
+    # Assert we did change the system.
+    assert(len(insts) == 1)
+    # Make sure we can connect
+    topology.standalone.open()
+    # Make sure we can start stop.
+    topology.standalone.stop()
+    topology.standalone.start()
+    # Okay, actually remove the instance
+    insts = topology.standalone.list(all=True)
+    remove_ds_instance(topology.standalone)
+    if (len(insts) == 1):
+        res = run_cmd(["semanage", "fcontext", "--list", "-C"])
+        assert not "dirsrv_" in res
+        res = run_cmd(["semanage", "port", "--list", "-C"])
+        assert not "ldap_port_t" in res
+
+
 def write_file(fname, pwd, is_runnable, lines):
     log.debug(f'Creating file {fname} with:')
     with open(fname, 'wt') as f:
@@ -180,7 +287,7 @@ def test_setup_ds_as_non_root():
     try:
         pwd_nru = pwd.getpwnam(NON_ROOT_USER)
     except KeyError:
-        subprocess.run(('/usr/sbin/useradd', NON_ROOT_USER))
+        subprocess.run(('/usr/sbin/useradd', NON_ROOT_USER), check=True)
         pwd_nru = pwd.getpwnam(NON_ROOT_USER)
 
     # Create a temporary directory
