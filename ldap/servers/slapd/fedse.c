6669305f38608f7ce4ad65a2ee2a82faf243b4a7
Simon Pichugin <spichugi@redhat.com>
2022-10-04 19:31:20 -0700

    Issue 5356 - Update Cargo.lock and bootstrap PBKDF2-SHA512
    Description: We've changed our default password storage scheme to
    PBKDF2-SHA512. Change the bootstrap logic to use the scheme when
    Rust is enabled.
    
    Related: https://github.com/389ds/389-ds-base/issues/5356
    
    Reviewed by: ?

--- ldap/servers/slapd/fedse.c
+++ ldap/servers/slapd/fedse.c
@@ -256,19 +256,6 @@ static const char *internal_entries[] =
         "nsslapd-pluginVersion: none\n"
         "nsslapd-pluginVendor: 389 Project\n"
         "nsslapd-pluginDescription: PBKDF2-SHA256\n",
-
-        "dn: cn=PBKDF2-SHA512,cn=Password Storage Schemes,cn=plugins,cn=config\n"
-        "objectclass: top\n"
-        "objectclass: nsSlapdPlugin\n"
-        "cn: PBKDF2-SHA512\n"
-        "nsslapd-pluginpath: libpwdchan-plugin\n"
-        "nsslapd-plugininitfunc: pwdchan_pbkdf2_sha512_plugin_init\n"
-        "nsslapd-plugintype: pwdstoragescheme\n"
-        "nsslapd-pluginenabled: on\n"
-        "nsslapd-pluginId: PBKDF2-SHA512\n"
-        "nsslapd-pluginVersion: none\n"
-        "nsslapd-pluginVendor: 389 Project\n"
-        "nsslapd-pluginDescription: PBKDF2-SHA512\n",
 #endif
 };
 
