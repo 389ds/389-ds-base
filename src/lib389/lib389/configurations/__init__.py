adba7e8e2ed2bd759cd4640e167413d71f254bd1
William Brown <william@blackhats.net.au>
2022-10-06 11:15:42 +1000

    Issue 5476 - RFE - add memberUid read aci by default
    Bug Description: Some legacy client issue searches for
    (|(member=...)(memberUid=...)). Because of how our
    aci's work, the lack of access to memberUid (even if
    empty) would block the legitimate search to member=.
    
    Fix Description: Allow memberUid to be searched by
    default.
    
    fixes: https://github.com/389ds/389-ds-base/issues/5476
    
    Author: William Brown <william@blackhats.net.au>
    
    Review by: ???

--- src/lib389/lib389/configurations/__init__.py
+++ src/lib389/lib389/configurations/__init__.py
@@ -11,17 +11,22 @@ from lib389._constants import INSTALL_LATEST_CONFIG
 from .config_001003006 import c001003006, c001003006_sample_entries
 from .config_001004000 import c001004000, c001004000_sample_entries
 from .config_001004002 import c001004002, c001004002_sample_entries
+from .config_002003000 import c002003000, c002003000_sample_entries
 
 
 def get_config(version):
     # We do this to avoid test breaking on older version that may
     # not expect the new default layout.
+    if (version == INSTALL_LATEST_CONFIG and ds_is_newer('2.3.0')):
+        return c002003000
     if (version == INSTALL_LATEST_CONFIG and ds_is_newer('1.4.2')):
         return c001004002
     elif (version == INSTALL_LATEST_CONFIG and ds_is_newer('1.4.0')):
         return c001004000
     elif (version == INSTALL_LATEST_CONFIG):
         return c001003006
+    elif (version == '002003000' and ds_is_newer('2.3.0')):
+        return c001004002
     elif (version == '001004002' and ds_is_newer('1.4.2')):
         return c001004002
     elif (version == '001004000' and ds_is_newer('1.4.0')):
@@ -33,7 +38,9 @@ def get_config(version):
 
 def get_sample_entries(version):
     if (version == INSTALL_LATEST_CONFIG):
-        return c001004002_sample_entries
+        return c002003000_sample_entries
+    elif (version == '002003000'):
+        return c002003000_sample_entries
     elif (version == '001004002'):
         return c001004002_sample_entries
     elif (version == '001004000'):
