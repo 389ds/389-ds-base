e70d77d09a28d62195c434be89f12f9f8daee35e
Simon Pichugin <spichugi@redhat.com>
2022-11-17 08:31:50 -0800

    Issue 5534 - Add copyright text to the repository files
    Description: We need to have copyright texts around our files and
    some of it is missing. This commit adds the copyright to tests and
    lib389.
    Also, add an automatic generator in the create_test.py script.
    
    Fixes: https://github.com/389ds/389-ds-base/issues/5534
    
    Reviewed by: @mreynolds389, @progier389 (Thanks!)

--- src/lib389/lib389/tests/cli/conf_plugins/automember_test.py
+++ src/lib389/lib389/tests/cli/conf_plugins/automember_test.py
@@ -1,3 +1,11 @@
+# --- BEGIN COPYRIGHT BLOCK ---
+# Copyright (C) 2017 alisha17 <anejaalisha@yahoo.com>
+# All rights reserved.
+#
+# License: GPL (version 3 or any later version).
+# See LICENSE for details.
+# --- END COPYRIGHT BLOCK ---
+#
 import pytest
 import ldap
 
