31353dfcaf20f82081da78a87a3d6b45f62c685e
Mark Reynolds <mreynolds@redhat.com>
2023-02-13 16:21:13 -0500

    Issue 5646 - CLI/UI - do not hardcode password storage schemes
    Description:  Previously all the password storage schemes were hardcoded
                  in the UI.  Now dsconf can list all the current schemes the
                  server supports
    
    relates: https://github.com/389ds/389-ds-base/issues/5636
    
    Reviewed by: spichugi(Thanks!)

--- src/lib389/lib389/password_plugins.py
+++ src/lib389/lib389/password_plugins.py
@@ -1,12 +1,13 @@
 # --- BEGIN COPYRIGHT BLOCK ---
 # Copyright (C) 2018 William Brown <william@blackhats.net.au>
+# Copyright (C) 2023 Red Hat, Inc.
 # All rights reserved.
 #
 # License: GPL (version 3 or any later version).
 # See LICENSE for details.
 # --- END COPYRIGHT BLOCK ---
 
-from lib389.plugins import Plugin
+from lib389.plugins import Plugin, Plugins
 from lib389._constants import DN_PWDSTORAGE_SCHEMES
 
 
@@ -53,3 +54,12 @@ class CRYPTPlugin(PasswordPlugin):
 class SSHAPlugin(PasswordPlugin):
     def __init__(self, instance, dn=f'cn=SSHA,{DN_PWDSTORAGE_SCHEMES}'):
         super(SSHAPlugin, self).__init__(instance, dn)
+
+
+class PasswordPlugins(Plugins):
+    def __init__(self, instance):
+        super(PasswordPlugins, self).__init__(instance=instance)
+        self._objectclasses = ['nsSlapdPlugin']
+        self._filterattrs = ['cn']
+        self._childobject = PasswordPlugin
+        self._basedn = DN_PWDSTORAGE_SCHEMES
