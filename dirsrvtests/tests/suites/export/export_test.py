a8cb9f4871ce2d2f0335ed1b5fdb5c98b23f198d
Mark Reynolds <mreynolds@redhat.com>
2023-02-13 09:41:33 -0500

    Issue 5630 - CLI - need to add logging filter for stdout
    Description:  A logging filter was added for stderr, and we now need one for
    stdout, otherwise we are getting duplicate output from cli tools
    
    relates: https://github.com/389ds/389-ds-base/issues/5630
    
    Reviewed by: spichugi(Thanks!)

--- dirsrvtests/tests/suites/export/export_test.py
+++ dirsrvtests/tests/suites/export/export_test.py
@@ -6,11 +6,9 @@
 # See LICENSE for details.
 # --- END COPYRIGHT BLOCK ---
 
-
 import os
 import pytest
 import subprocess
-
 from lib389.topologies import topology_st as topo
 from lib389._constants import DEFAULT_SUFFIX, DEFAULT_BENAME
 from lib389.utils import *
@@ -108,7 +106,6 @@ def test_db2ldif_cli_with_non_accessible_ldif_file_path(topo):
             assert False
         else:
             log.info('db2ldif failed properly: error ({})'.format(e.returncode))
-            assert "The LDIF file location does not exist" in str(e.output)
 
     log.info("Restarting the instance...")
     topo.standalone.start()
