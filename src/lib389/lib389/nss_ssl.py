c0b8a21cbf8592d75fdd42a4094edf4cb1609cb5
Mark Reynolds <mreynolds@redhat.com>
2023-05-11 10:06:26 -0400

    Issue 5768 - CLI/UI - cert checks are too strict, and other issues
    Description:
    
    The certificate type checks for CA/server break if there are no certificate
    extensions set (use openssl in that case to gather the info instead).
    dscontainter needed to be updated for new cert checks, and UI adding certs
    improvements.
    
    relates: https://github.com/389ds/389-ds-base/issues/5768
    
    Reviewed by: spichugi(Thanks!)

--- src/lib389/lib389/nss_ssl.py
+++ src/lib389/lib389/nss_ssl.py
@@ -1076,7 +1076,6 @@ only.
         except subprocess.CalledProcessError as e:
             raise ValueError(e.output.decode('utf-8').rstrip())
 
-
     def display_cert_details(self, nickname):
         cmd = [
             '/usr/bin/certutil',
@@ -1093,7 +1092,6 @@ only.
             raise ValueError(e.output.decode('utf-8').rstrip())
 
         return ensure_str(result)
-
 
     def get_cert_details(self, nickname):
         """Get the trust flags, subject DN, issuer, and expiration date
