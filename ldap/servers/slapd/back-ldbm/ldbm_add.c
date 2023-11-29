445a881d91eb8599eb2261899bf0ded3c97faea4
tbordaz <tbordaz@redhat.com>
2023-11-29 13:46:57 +0100

    Issue 5944 - Reversion of the entry cache should be limited to BETXN plugin failures (#5994)
    Bug description:
    	During an update if an BETXN plugin fails the full TXN is aborted and
    	the DB returns to the previous state. However potential internal
    	updates, done by BETXN plugins, are also applied on the entry cache.
    	Some entries in the entry cache are left in a state that does not
    	reflect the DB state. To prevent this mismatch, upon BETXN failure,
    	the fix https://pagure.io/389-ds-base/issue/50260 reverts some entries
    	in the entry cache .
    
    	The problem is that reversion is not limited to the cases of BETXN
    	failures that was the initial goal. So a "regular" error like schema
    	violation could trigger revert_cache
    
    Fix description:
    	The fix flags if the failure is due to BETXN failures and
    	trigger revert_cache only in that case
    
    relates: #5944
    
    Reviewed by: Pierre Rogier (Thanks!)

--- ldap/servers/slapd/back-ldbm/ldbm_add.c
+++ ldap/servers/slapd/back-ldbm/ldbm_add.c
@@ -98,6 +98,7 @@ ldbm_back_add(Slapi_PBlock *pb)
     int op_id;
     int result_sent = 0;
     int32_t parent_op = 0;
+    int32_t betxn_callback_fails = 0; /* if a BETXN fails we need to revert entry cache */
     struct timespec parent_time;
 
     if (slapi_pblock_get(pb, SLAPI_CONN_ID, &conn_id) < 0) {
@@ -919,6 +920,9 @@ ldbm_back_add(Slapi_PBlock *pb)
                 slapi_pblock_get(pb, SLAPI_PB_RESULT_TEXT, &ldap_result_message);
                 slapi_log_err(SLAPI_LOG_DEBUG, "ldbm_back_add", "SLAPI_PLUGIN_BE_TXN_PRE_ADD_FN plugin failed: %d\n",
                               ldap_result_code ? ldap_result_code : retval);
+                if (retval) {
+                    betxn_callback_fails = 1;
+                }
                 goto error_return;
             }
         }
@@ -1229,11 +1233,15 @@ ldbm_back_add(Slapi_PBlock *pb)
             slapi_pblock_set(pb, SLAPI_PLUGIN_OPRETURN, ldap_result_code ? &ldap_result_code : &retval);
         }
         slapi_pblock_get(pb, SLAPI_PB_RESULT_TEXT, &ldap_result_message);
+        if (retval) {
+            betxn_callback_fails = 1;
+        }
         goto error_return;
     }
 
     retval = plugin_call_mmr_plugin_postop(pb, NULL,SLAPI_PLUGIN_BE_TXN_POST_ADD_FN);
     if (retval) {
+        betxn_callback_fails = 1;
         ldbm_set_error(pb, retval, &ldap_result_code, &ldap_result_message);
         goto error_return;
     }
@@ -1326,9 +1334,15 @@ diskfull_return:
                     slapi_pblock_set(pb, SLAPI_PLUGIN_OPRETURN, &opreturn);
                 }
                 slapi_pblock_get(pb, SLAPI_PB_RESULT_TEXT, &ldap_result_message);
+                if (retval) {
+                    betxn_callback_fails = 1;
+                }
             }
             /* the repl postop needs to be called for aborted operations */
             retval = plugin_call_mmr_plugin_postop(pb, NULL,SLAPI_PLUGIN_BE_TXN_POST_ADD_FN);
+            if (retval) {
+                betxn_callback_fails = 1;
+            }
             if (addingentry) {
                 if (inst && cache_is_in_cache(&inst->inst_cache, addingentry)) {
                     CACHE_REMOVE(&inst->inst_cache, addingentry);
@@ -1366,7 +1380,7 @@ diskfull_return:
         }
 
         /* Revert the caches if this is the parent operation */
-        if (parent_op) {
+        if (parent_op && betxn_callback_fails) {
             revert_cache(inst, &parent_time);
         }
     }
