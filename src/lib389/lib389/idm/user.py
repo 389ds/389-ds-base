05eac41a180c01f63fae3d82110605f2e878b689
Simon Pichugin <spichugi@redhat.com>
2024-02-16 13:52:36 -0800

    Issue 6067 - Improve dsidm CLI No Such Entry handling (#6079)
    Description: Add additional error processing to dsidm CLI tool for when basedn
    or OU subentries are absent.
    
    Related: https://github.com/389ds/389-ds-base/issues/6067
    
    Reviewed by: @vashirov (Thanks!)

--- src/lib389/lib389/idm/user.py
+++ src/lib389/lib389/idm/user.py
@@ -1,6 +1,6 @@
 # --- BEGIN COPYRIGHT BLOCK ---
 # Copyright (C) 2016, William Brown <william at blackhats.net.au>
-# Copyright (C) 2023 Red Hat, Inc.
+# Copyright (C) 2024 Red Hat, Inc.
 # All rights reserved.
 #
 # License: GPL (version 3 or any later version).
@@ -23,6 +23,7 @@ MUST_ATTRIBUTES = [
     'homeDirectory',
 ]
 RDN = 'uid'
+DEFAULT_BASEDN_RDN = 'ou=People'
 
 TEST_USER_PROPERTIES = {
     'uid': 'testuser',
@@ -201,7 +202,7 @@ class UserAccounts(DSLdapObjects):
     :type rdn: str
     """
 
-    def __init__(self, instance, basedn, rdn='ou=People'):
+    def __init__(self, instance, basedn, rdn=DEFAULT_BASEDN_RDN):
         super(UserAccounts, self).__init__(instance)
         self._objectclasses = [
             'account',
