de82df01594ef3a6553f87f9578b7abca14d99f9
James Chapman <jachapma@redhat.com>
2024-02-21 12:43:03 +0000

    Issue 6103 - New connection timeout error breaks errormap (#6104)
    Bug description: A recent addition to the connection disconnect error
    messaging, conflicts with how errormap.c maps error codes/strings.
    
    Fix description: errormap expects error codes/strings to be in ascending
    order. Moved the new error code to the bottom of the list.
    
    Relates: https://github.com/389ds/389-ds-base/issues/6103
    
    Reviewed by: @droideck. @progier389  (Thank you)

--- ldap/servers/slapd/disconnect_error_strings.h
+++ ldap/servers/slapd/disconnect_error_strings.h
@@ -14,7 +14,8 @@
 /* disconnect_error_strings.h
  *
  * Strings describing the errors used in logging the reason a connection
- * was closed.
+ * was closed. Ensure definitions are in the same order as the error codes
+ * defined in disconnect_errors.h
  */
 #ifndef __DISCONNECT_ERROR_STRINGS_H_
 #define __DISCONNECT_ERROR_STRINGS_H_
@@ -33,6 +34,6 @@ ER2(SLAPD_DISCONNECT_UNBIND, "U1")
 ER2(SLAPD_DISCONNECT_POLL, "P2")
 ER2(SLAPD_DISCONNECT_NTSSL_TIMEOUT, "T2")
 ER2(SLAPD_DISCONNECT_SASL_FAIL, "S1")
-
+ER2(SLAPD_DISCONNECT_PAGED_SEARCH_LIMIT, "T3")
 
 #endif /* __DISCONNECT_ERROR_STRINGS_H_ */
