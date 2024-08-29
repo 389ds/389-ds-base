e633e341b5b73332d0ac85033f815a32cbabc926
tbordaz <tbordaz@redhat.com>
2024-08-29 10:49:57 +0200

    Issue 6312 - In branch 2.5, healthcheck report an invalid warning regarding BDB deprecation (#6313)
    Bug description:
    	during healthcheck, _lint_backend_implementation checks that
    	the instance is not running a BDB backend.
    	This check only applies for instance after 3.0.0
    
    Fix description:
    	If the instance is newer than 3.0.0 the health check
    	just returns
    
    relates: #6312
    
    Reviewed by:

--- src/lib389/lib389/backend.py
+++ src/lib389/lib389/backend.py
@@ -14,7 +14,7 @@ from lib389._constants import DN_LDBM, DN_CHAIN, DN_PLUGIN, DEFAULT_BENAME
 from lib389.properties import BACKEND_OBJECTCLASS_VALUE, BACKEND_PROPNAME_TO_ATTRNAME, BACKEND_CHAIN_BIND_DN, \
                               BACKEND_CHAIN_BIND_PW, BACKEND_CHAIN_URLS, BACKEND_PROPNAME_TO_ATTRNAME, BACKEND_NAME, \
                               BACKEND_SUFFIX, BACKEND_SAMPLE_ENTRIES, TASK_WAIT
-from lib389.utils import normalizeDN, ensure_str, assert_c
+from lib389.utils import normalizeDN, ensure_str, assert_c, ds_is_newer
 from lib389 import Entry
 
 # Need to fix this ....
@@ -513,7 +513,7 @@ class Backend(DSLdapObject):
 
     def _lint_backend_implementation(self):
         backend_impl = self._instance.get_db_lib()
-        if backend_impl == 'bdb':
+        if backend_impl == 'bdb' and ds_is_newer('3.0.0', instance=self._instance):
             result = DSBLE0006
             result['items'] = [self.lint_uid()]
             yield result
