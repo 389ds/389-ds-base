c9080eaf61334c2ff8703f3cf202bee659c10ae3
progier389 <progier@redhat.com>
2025-03-24 10:43:21 +0100

    Issue 6680 - instance read-only mode is broken (#6681)
    Read only mode is broken because some plugins fails to starts as they are not able to create/updates some entries in the dse backend.
    Solution is to allow interrnal operations to write in dse.backend but not modify the dse.ldif (except for the special case when trying to modify nsslapd-readonly flags (to be allowed to set/unset the readonly mode)
    
    Issue: #6680
    
    Reviewed by: @droideck, @tbordaz (thanks!)

--- dirsrvtests/tests/suites/config/regression_test.py
+++ dirsrvtests/tests/suites/config/regression_test.py
@@ -28,6 +28,8 @@ CUSTOM_MEM = '9100100100'
 IDLETIMEOUT = 5
 DN_TEST_USER = f'uid={TEST_USER_PROPERTIES["uid"]},ou=People,{DEFAULT_SUFFIX}'
 
+RO_ATTR = 'nsslapd-readonly'
+
 
 @pytest.fixture(scope="module")
 def idletimeout_topo(topo, request):
@@ -188,3 +190,61 @@ def test_idletimeout(idletimeout_topo, dn, expected_result):
     except ldap.SERVER_DOWN:
         result = True
     assert expected_result == result
+
+
+def test_instance_readonly_mode(topo):
+    """Check that readonly mode is supported
+
+    :id: 34d2e28e-04d7-11f0-b0cf-482ae39447e5
+    :setup: Standalone Instance
+    :steps:
+        1. Set readonly mode
+        2. Stop the instance
+        3. Get dse.ldif modification time
+        4. Start the instance
+        5. Get dse.ldif modification time
+        6. Check that modification time has not changed
+        7. Check that readonly mode is set
+        8. Try to modify another config attribute
+        9. Unset readonly mode
+        10. Restart the instance
+        11. Check that modification time has not changed
+        12. Check that modification time has changed
+        13. Check that readonly mode is unset
+        14. Try to modify another config attribute
+    :expectedresults:
+        1. Success
+        2. Success
+        3. Success
+        4. Success
+        5. Success
+        6. Success
+        7. Success
+        8. Should get ldap.UNWILLING_TO_PERFORM exception
+        9. Success
+        10. Success
+        11. Success
+        12. Success
+        13. Success
+        14. Success
+    """
+
+    inst = topo.standalone
+    dse_path = f'{topo.standalone.get_config_dir()}/dse.ldif'
+    inst.config.replace(RO_ATTR, 'on')
+    inst.stop()
+    dse_mtime = os.stat(dse_path).st_mtime
+    inst.start()
+    new_dse_mtime = os.stat(dse_path).st_mtime
+    assert dse_mtime == new_dse_mtime
+    assert inst.config.get_attr_val_utf8(RO_ATTR) == "on"
+    attr = 'nsslapd-errorlog-maxlogsize'
+    val = inst.config.get_attr_val_utf8(attr)
+    with pytest.raises(ldap.UNWILLING_TO_PERFORM):
+        inst.config.replace(attr, val)
+    inst.config.replace(RO_ATTR, 'off')
+    inst.restart()
+    new_dse_mtime = os.stat(dse_path).st_mtime
+    assert dse_mtime != new_dse_mtime
+    assert inst.config.get_attr_val_utf8(RO_ATTR) == "off"
+    inst.config.replace(attr, val)
