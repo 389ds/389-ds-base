2b85af7c56ab35eadf677b3bb49a526fe8f68116
Viktor Ashirov <vashirov@redhat.com>
2025-04-11 13:09:44 +0200

    Issue 6603 - Release tarballs ship a different Cargo.lock
    Bug Description:
    During release action `Cargo.lock` gets updated, has newer packages than
    `Cargo.lock` committed in the repository.
    Because of that upstream patches that update `Cargo.lock` do not apply
    cleanly to the release tarball.
    
    Fix Description:
    Remove `cargo update` from the `download-cargo-dependencies` target. For
    updating `Cargo.lock` there is a separate `update-cargo-dependencies`
    target that should be used instead.
    Additionally, fix `dist-bz2` target to generate tarball with a proper
    name when `TAG` variable is not defined.
    
    Fixes: https://github.com/389ds/389-ds-base/issues/6603
    
    Reviewed by: @progier389 (Thanks!)

--- rpm.mk
+++ rpm.mk
@@ -14,7 +14,7 @@ BUNDLE_JEMALLOC = 1
 NODE_MODULES_TEST = src/cockpit/389-console/node_modules/webpack
 NODE_MODULES_PATH = src/cockpit/389-console/
 CARGO_PATH = src/
-GIT_TAG = ${TAG}
+GIT_TAG = $(if $(TAG),$(TAG),$(PACKAGE)-$(RPM_VERSION)$(VERSION_PREREL))
 
 # Some sanitizers are supported only by clang
 CLANG_ON = 0
@@ -41,7 +41,6 @@ update-cargo-dependencies:
 	cargo update --manifest-path=./src/Cargo.toml
 
 download-cargo-dependencies:
-	cargo update --manifest-path=./src/Cargo.toml
 	cargo vendor --manifest-path=./src/Cargo.toml
 	cargo fetch --manifest-path=./src/Cargo.toml
 	tar -czf vendor.tar.gz vendor
