f93d23b2b24b7535746a50129481ee01ff0ac11f
progier389 <progier@redhat.com>
2025-05-05 15:15:14 +0200

    Entry cache eviction improvment

--- src/lib389/lib389/backend.py
+++ src/lib389/lib389/backend.py
@@ -1248,6 +1248,7 @@ class BackendSuffixView(CompositeDSLdapObject):
         be_args = [
             'nsslapd-cachememsize',
             'nsslapd-cachesize',
+            'nsslapd-cache-preserved-entries',
             'nsslapd-dncachememsize',
             'nsslapd-readonly',
             'nsslapd-require-index',
