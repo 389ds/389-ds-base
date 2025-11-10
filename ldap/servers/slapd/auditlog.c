75d70fda8846bfabd626696eec47a6880aa1c9cf
Viktor Ashirov <vashirov@redhat.com>
2025-11-10 13:20:28 +0100

    Issue 7049 - RetroCL plugin generates invalid LDIF
    Bug Description:
    When a replicated modification marked with LDAP_MOD_IGNORE is logged,
    `changes` attribute contains invalid LDIF:
    
    ```
    replace: modifiersName
    modifiersName: cn=MemberOf Plugin,cn=plugins,cn=config
    -
    modifyTimestamp: 20250903092211Z
    -
    ```
    Line `replace: modifyTimestamp` is missing.
    
    A similar issue is present in audit log:
    ```
    time: 20251031064114
    dn: ou=tuser,dc=example,dc=com
    result: 0
    changetype: modify
    add: objectClass
    objectClass: nsMemberOf
    -
    replace: modifiersName
    modifiersName: cn=MemberOf Plugin,cn=plugins,cn=config
    -
    -
    ```
    Dash separator is logged, while the operation is not.
    This issue is not present wheh JSON format is used.
    
    Fix Description:
    * retrocl_po.c: add a default case to skip the entire modification if it
      has LDAP_MOD_IGNORE flag.
    * auditlog.c: write the dash separator only if operation type is not
      LDAP_MOD_IGNORE
    
    Fixes: https://github.com/389ds/389-ds-base/issues/7049
    
    Reviewed by: @progier389 (Thanks!)

--- ldap/servers/slapd/auditlog.c
+++ ldap/servers/slapd/auditlog.c
@@ -854,8 +854,8 @@ write_audit_file(
                         slapi_ch_free((void **)&buf);
                     }
                 }
+                addlenstr(l, "-\n");
             }
-            addlenstr(l, "-\n");
         }
         break;
 
