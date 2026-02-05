ea1b51df9a698915b972923357caba1c78dcc82f
Viktor Ashirov <vashirov@redhat.com>
2026-02-05 12:17:06 +0100

    Issue 7223 - Add upgrade function to remove nsIndexIDListScanLimit from parentid
    Description:
    Add `upgrade_remove_index_scanlimit()` function that removes the
    nsIndexIDListScanLimit attribute from parentid index configuration
    if present.
    
    This attribute was incorrectly added by a previous version and can
    cause issues with index configuration. The upgrade function runs
    automatically on server startup and removes the attribute if found.
    
    Relates: https://github.com/389ds/389-ds-base/issues/7223
    
    Reviewed by: @progier389, @tbordaz, @droideck (Thanks!)

--- ldap/servers/slapd/upgrade.c
+++ ldap/servers/slapd/upgrade.c
@@ -331,6 +331,107 @@ upgrade_remove_subtree_rename(void)
 }
 
 /*
+ * Remove nsIndexIDListScanLimit from parentid index configuration.
+ *
+ * This attribute was incorrectly added by a previous version and can
+ * cause issues with index configuration. Remove it if present.
+ */
+static upgrade_status
+upgrade_remove_index_scanlimit(void)
+{
+    struct slapi_pblock *pb = slapi_pblock_new();
+    Slapi_Entry **backends = NULL;
+    const char *be_base_dn = "cn=ldbm database,cn=plugins,cn=config";
+    const char *be_filter = "(objectclass=nsBackendInstance)";
+    const char *attrs_to_check[] = {"parentid", NULL};
+    upgrade_status uresult = UPGRADE_SUCCESS;
+
+    /* Search for all backend instances */
+    slapi_search_internal_set_pb(
+            pb, be_base_dn,
+            LDAP_SCOPE_ONELEVEL,
+            be_filter, NULL, 0, NULL, NULL,
+            plugin_get_default_component_id(), 0);
+    slapi_search_internal_pb(pb);
+    slapi_pblock_get(pb, SLAPI_PLUGIN_INTOP_SEARCH_ENTRIES, &backends);
+
+    if (backends) {
+        for (size_t be_idx = 0; backends[be_idx] != NULL; be_idx++) {
+            const char *be_dn = slapi_entry_get_dn_const(backends[be_idx]);
+            const char *be_name = slapi_entry_attr_get_ref(backends[be_idx], "cn");
+            if (!be_dn || !be_name) {
+                continue;
+            }
+
+            for (size_t attr_idx = 0; attrs_to_check[attr_idx] != NULL; attr_idx++) {
+                const char *attr_name = attrs_to_check[attr_idx];
+                struct slapi_pblock *idx_pb = slapi_pblock_new();
+                Slapi_Entry **idx_entries = NULL;
+                char *idx_dn = slapi_create_dn_string("cn=%s,cn=index,%s",
+                                                       attr_name, be_dn);
+                char *idx_filter = "(objectclass=nsIndex)";
+
+                if (!idx_dn) {
+                    slapi_pblock_destroy(idx_pb);
+                    continue;
+                }
+
+                slapi_search_internal_set_pb(
+                        idx_pb, idx_dn,
+                        LDAP_SCOPE_BASE,
+                        idx_filter, NULL, 0, NULL, NULL,
+                        plugin_get_default_component_id(), 0);
+                slapi_search_internal_pb(idx_pb);
+                slapi_pblock_get(idx_pb, SLAPI_PLUGIN_INTOP_SEARCH_ENTRIES, &idx_entries);
+
+                if (idx_entries && idx_entries[0]) {
+                    /* Check if nsIndexIDListScanLimit is present */
+                    if (slapi_entry_attr_get_ref(idx_entries[0], "nsIndexIDListScanLimit") != NULL) {
+                        /* Remove nsIndexIDListScanLimit */
+                        Slapi_PBlock *mod_pb = slapi_pblock_new();
+                        Slapi_Mods smods;
+                        int rc;
+
+                        slapi_mods_init(&smods, 1);
+                        slapi_mods_add(&smods, LDAP_MOD_DELETE, "nsIndexIDListScanLimit", 0, NULL);
+
+                        slapi_modify_internal_set_pb(
+                                mod_pb, idx_dn,
+                                slapi_mods_get_ldapmods_byref(&smods),
+                                NULL, NULL,
+                                plugin_get_default_component_id(), 0);
+                        slapi_modify_internal_pb(mod_pb);
+                        slapi_pblock_get(mod_pb, SLAPI_PLUGIN_INTOP_RESULT, &rc);
+
+                        if (rc == LDAP_SUCCESS) {
+                            slapi_log_err(SLAPI_LOG_NOTICE, "upgrade_remove_index_scanlimit",
+                                    "Removed 'nsIndexIDListScanLimit' from index '%s' in backend '%s'\n",
+                                    attr_name, be_name);
+                        } else if (rc != LDAP_NO_SUCH_ATTRIBUTE) {
+                            slapi_log_err(SLAPI_LOG_ERR, "upgrade_remove_index_scanlimit",
+                                    "Failed to remove 'nsIndexIDListScanLimit' from index '%s' in backend '%s': error %d\n",
+                                    attr_name, be_name, rc);
+                        }
+
+                        slapi_mods_done(&smods);
+                        slapi_pblock_destroy(mod_pb);
+                    }
+                }
+
+                slapi_ch_free_string(&idx_dn);
+                slapi_free_search_results_internal(idx_pb);
+                slapi_pblock_destroy(idx_pb);
+            }
+        }
+    }
+
+    slapi_free_search_results_internal(pb);
+    slapi_pblock_destroy(pb);
+
+    return uresult;
+}
+
+/*
  * Check if parentid/ancestorid indexes are missing the integerOrderingMatch
  * matching rule.
  *
@@ -646,6 +747,10 @@ upgrade_server(void)
     }
 
     if (upgrade_pam_pta_default_config() != UPGRADE_SUCCESS) {
+        return UPGRADE_FAILURE;
+    }
+
+    if (upgrade_remove_index_scanlimit() != UPGRADE_SUCCESS) {
         return UPGRADE_FAILURE;
     }
 
