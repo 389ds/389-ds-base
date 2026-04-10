665b4932e3d5dfe0120ac55f2d4e1d98ffbe1718
Thierry Bordaz <tbordaz@redhat.com>
2026-04-10 16:11:49 +0200

    Issue 7400 - 389-ds handles " *" as "*" in ldapsearch command and ignores space character
    Bug description:
    	A initial substring search containing trailing spaces may return
    	improper matching entries.
    	The RC is that the normalization of values trim leading and trailing
    	spaces.
    
    Fix description:
    	When trimming (spaces) is requested the caller should specify if
    	leading/trailing/both spaces should be trimmed
    
    fixes: #7400
    
    Assisted by: Cursor
    
    Reviewed by:

--- ldap/servers/plugins/syntaxes/tel.c
+++ ldap/servers/plugins/syntaxes/tel.c
@@ -289,6 +289,17 @@ tel_normalize(
     int trim_spaces,
     char **alt)
 {
-    value_normalize_ext(s, SYNTAX_TEL | SYNTAX_CIS, trim_spaces, alt);
+    int trim_mask = 0;
+    if (trim_spaces == COMPATIBLE_NOT_TRIM_SPACES) {
+        /* value 0x00 */
+        trim_mask = NO_TRIM_SHRINK_BLANK;
+    } else if (trim_spaces == COMPATIBLE_TRIM_SPACES) {
+        /* value 0x01 */
+        trim_mask = COMPATIBLE_TRIM_MASK;
+    } else {
+        /* value 0x10-0x80 */
+        trim_mask = trim_spaces;
+    }
+    value_normalize_ext(s, SYNTAX_TEL | SYNTAX_CIS, trim_mask, alt);
     return;
 }
