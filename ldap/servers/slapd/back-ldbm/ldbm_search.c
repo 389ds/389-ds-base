892a3f387353b5f7ed10e9becc9e0dea01dfa73b
Viktor Ashirov <vashirov@redhat.com>
2026-04-28 14:09:45 +0200

    Issue 7155 - build_candidate_list - Database error 11 with range search (#7156)
    Range search behavior discrepancy when hitting nsslapd-rangelookthroughlimit whether filter_candidates_ext returns a short or long list of candidates:
    Search is successful if the candidate list is long but fail if it is short
    Error should be ignored in both case
    
    Issue: #7155
    
    Reviewed by: @tbordaz (Thanks!)
    (cherry picked from commit 8c2229a02e0d8d6ef8573e014073f27d4cef523c)

--- ldap/servers/slapd/back-ldbm/ldbm_search.c
+++ ldap/servers/slapd/back-ldbm/ldbm_search.c
@@ -1557,6 +1557,8 @@ subtree_candidates(
             idl_free(&tmp);
             idl_free(&descendants);
         }
+    } else if (candidates != NULL) {
+        *err = LDAP_SUCCESS;
     }
 
     return (candidates);
