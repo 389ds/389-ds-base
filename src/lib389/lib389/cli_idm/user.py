8c6e85f23f08eeec1cce6ed398841763b5332fa3
Mark Reynolds <mreynolds@redhat.com>
2026-04-29 16:20:48 -0400

    Issue 7464 - CLI - allow dsidm to work with other user types
    Description:
    
    In the UI you can create these entry types:
    
    - Posix (default for dsidm)
    - Basic (similar to nsUserAccount but without posix requirements)
    - Traditional (old style entries from DS 1.3)
    - Service account
    
    Add a user "--user-type" option to dsiadm users to specifiy these optional types.
    The default is stil "posix".
    
    relates: https://github.com/389ds/389-ds-base/issues/7464
    
    reviewed by: spichugi & progier(Thanks!!)
    
    Update argparse to correctly handle different user types when creating a new entry.
    
    Thanks Simon for the assist on this one!

--- src/lib389/lib389/cli_idm/user.py
+++ src/lib389/lib389/cli_idm/user.py
@@ -1,13 +1,21 @@
 # --- BEGIN COPYRIGHT BLOCK ---
 # Copyright (C) 2016, William Brown <william at blackhats.net.au>
-# Copyright (C) 2023 Red Hat, Inc.
+# Copyright (C) 2026 Red Hat, Inc.
 # All rights reserved.
 #
 # License: GPL (version 3 or any later version).
 # See LICENSE for details.
 # --- END COPYRIGHT BLOCK ---
 
-from lib389.idm.user import nsUserAccount, nsUserAccounts
+from lib389.idm.user import (
+    nsUserAccount,
+    nsUserAccounts,
+    BasicUserAccount,
+    BasicUserAccounts,
+    TraditionalUserAccount,
+    TraditionalUserAccounts,
+)
+from lib389.idm.services import ServiceAccount, ServiceAccounts
 from lib389.cli_base import populate_attr_arguments, _generic_modify, CustomHelpFormatter
 from lib389.cli_idm import (
     _generic_list,
@@ -21,55 +29,83 @@ from lib389.cli_idm import (
     _warn,
     )
 
-SINGULAR = nsUserAccount
-MANY = nsUserAccounts
+DEFAULT_USER_TYPE = "posix"
+USER_TYPE_CHOICES = ['basic', 'posix', 'service', 'traditional']
+SINGULAR_DICT = {
+    None: nsUserAccount,
+    'posix': nsUserAccount,
+    'basic': BasicUserAccount,
+    'service': ServiceAccount,
+    'traditional': TraditionalUserAccount,
+}
+MANY_DICT = {
+    None: nsUserAccounts,
+    'posix': nsUserAccounts,
+    'basic': BasicUserAccounts,
+    'service': ServiceAccounts,
+    'traditional': TraditionalUserAccounts,
+}
+
 RDN = 'uid'
 
 
 # These are a generic specification, try not to tamper with them
 
 def list(inst, basedn, log, args):
-    _generic_list(inst, basedn, log.getChild('_generic_list'), MANY, args)
+    _generic_list(inst, basedn, log.getChild('_generic_list'), MANY_DICT[args.user_type], args)
 
 
 def get(inst, basedn, log, args):
     rdn = _get_arg( args.selector, msg="Enter %s to retrieve" % RDN)
-    _generic_get(inst, basedn, log.getChild('_generic_get'), MANY, rdn, args)
+    _generic_get(inst, basedn, log.getChild('_generic_get'), MANY_DICT[args.user_type], rdn, args)
 
 
 def get_dn(inst, basedn, log, args):
     dn = _get_arg( args.dn, msg="Enter dn to retrieve")
-    _generic_get_dn(inst, basedn, log.getChild('_generic_get_dn'), MANY, dn, args)
+    _generic_get_dn(inst, basedn, log.getChild('_generic_get_dn'), MANY_DICT[args.user_type], dn, args)
 
 
 def create(inst, basedn, log, args):
-    kwargs = _get_attributes(args, SINGULAR._must_attributes)
-    _generic_create(inst, basedn, log.getChild('_generic_create'), MANY, kwargs, args)
+    user_type = getattr(args, 'user_type', None) or DEFAULT_USER_TYPE
+    kwargs = _get_attributes(args, SINGULAR_DICT[user_type]._must_attributes)
+    _generic_create(inst, basedn, log.getChild('_generic_create'), MANY_DICT[args.user_type], kwargs, args)
 
 
 def delete(inst, basedn, log, args, warn=True):
     dn = _get_arg( args.dn, msg="Enter dn to delete")
     if warn:
-        _warn(dn, msg="Deleting %s %s" % (SINGULAR.__name__, dn))
-    _generic_delete(inst, basedn, log.getChild('_generic_delete'), SINGULAR, dn, args)
+        _warn(dn, msg="Deleting %s %s" % (SINGULAR_DICT[args.user_type].__name__, dn))
+    _generic_delete(inst, basedn, log.getChild('_generic_delete'), SINGULAR_DICT[args.user_type], dn, args)
 
 
 def modify(inst, basedn, log, args, warn=True):
     rdn = _get_arg( args.selector, msg="Enter %s to retrieve" % RDN)
-    _generic_modify(inst, basedn, log.getChild('_generic_modify'), MANY, rdn, args)
+    _generic_modify(inst, basedn, log.getChild('_generic_modify'), MANY_DICT[args.user_type], rdn, args)
 
 
 def rename(inst, basedn, log, args, warn=True):
     rdn = _get_arg( args.selector, msg="Enter %s to retrieve" % RDN)
-    _generic_rename(inst, basedn, log.getChild('_generic_rename'), MANY, rdn, args)
+    _generic_rename(inst, basedn, log.getChild('_generic_rename'), MANY_DICT[args.user_type], rdn, args)
 
 
-def create_parser(subparsers):
+def create_parser(subparsers, user_type=DEFAULT_USER_TYPE):
     user_parser = subparsers.add_parser('user',
                                         help='Manage posix users.  The organizationalUnit (by default "ou=people") needs '
                                              'to exist prior to managing users.')
-
     subcommands = user_parser.add_subparsers(help='action')
+    user_parser.add_argument('--user-type',
+                             choices=USER_TYPE_CHOICES,
+                             default=DEFAULT_USER_TYPE,
+                             help=f'''The type of user to manage.
+                                  "basic" is an entry based off of objectclasses:
+                                      nsPerson, nsAccount, and nsOrgPerson.
+                                  "posix" is based off of objectclasses:
+                                      nsPerson, nsAccount, nsOrgPerson, and posixAccount.
+                                  "service" is based off of objectclasses:
+                                      nsAccount and applicationProcess.
+                                  "traditional", aka legacy, is based off of objectclasses:
+                                      person, organizationalPerson, and inetOrgPerson.
+                                  The default user type is "{DEFAULT_USER_TYPE}"''')
 
     list_parser = subcommands.add_parser('list', help='list', formatter_class=CustomHelpFormatter)
     list_parser.set_defaults(func=list)
@@ -86,7 +122,7 @@ def create_parser(subparsers):
 
     create_user_parser = subcommands.add_parser('create', help='create', formatter_class=CustomHelpFormatter)
     create_user_parser.set_defaults(func=create)
-    populate_attr_arguments(create_user_parser, SINGULAR._must_attributes)
+    populate_attr_arguments(create_user_parser, SINGULAR_DICT[user_type]._must_attributes)
 
     modify_parser = subcommands.add_parser('modify', help='modify <add|delete|replace>:<attribute>:<value> ...', formatter_class=CustomHelpFormatter)
     modify_parser.set_defaults(func=modify)
