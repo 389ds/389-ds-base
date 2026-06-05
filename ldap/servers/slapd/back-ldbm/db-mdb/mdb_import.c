79654bfbdae25e33a62b40f1e78597aee2126f37
Viktor Ashirov <vashirov@redhat.com>
2026-06-05 11:45:45 +0200

    Issue 7539 - Server shutdown during online reindex may lead to data loss (#7540)
    Bug Description:
    When an online reindex task is aborted, for example by shutting down the
    server with SIGTERM while the reindex is in progress, the database data
    is lost, id2entry contains only 1 entry - RUV tombstone.
    Additionally, a SIGSEGV in `dbmdb_public_db_op` is observed when
    replication plugin tries to save the RUV state.
    
    Fix Description:
    When a reindex fails, don't close or delete the db. The backend is kept
    busy (unavailable for client operations) to prevent searches against
    incomplete indexes from returning wrong results. A CRIT message is
    logged advising the admin to perform an offline reindex to rebuild
    the indexes.
    
    Fixes: https://github.com/389ds/389-ds-base/issues/7539
    
    Reviewed by: @tbordaz, @progier389 (Thanks!)

--- ldap/servers/slapd/back-ldbm/db-mdb/mdb_import.c
+++ ldap/servers/slapd/back-ldbm/db-mdb/mdb_import.c
@@ -1011,11 +1011,26 @@ error:
         }
     }
     if (0 != ret) {
-        dblayer_instance_close(job->inst->inst_be);
-        if (!(job->flags & (FLAG_DRYRUN | FLAG_UPGRADEDNFORMAT_V1))) {
-            /* If not dryrun NOR upgradedn space */
-            /* if startcfg in the dry run mode, don't touch the db */
-            dbmdb_delete_instance_dir(be);
+        if (job->flags & FLAG_REINDEXING) {
+            /* Reindex only rebuilds secondary indexes from id2entry
+             * which is never modified during reindex. On failure we
+             * must NOT close or delete the instance, just bring the
+             * backend back online so the server can continue operating
+             * or shut down cleanly.
+             */
+            import_log_notice(job, SLAPI_LOG_CRIT, "dbmdb_public_dbmdb_import_main",
+                              "Reindex failed. Indexes may be incomplete."
+                              " The backend is unavailable until offline"
+                              " reindex is performed:"
+                              " stop the server, run 'dsctl <instance> db2index %s',"
+                              " then start the server.",
+                              inst->inst_name);
+        } else {
+            dblayer_instance_close(job->inst->inst_be);
+            if (!(job->flags & (FLAG_DRYRUN | FLAG_UPGRADEDNFORMAT_V1))) {
+                /* Not dryrun nor upgradedn - delete the half-imported db */
+                dbmdb_delete_instance_dir(be);
+            }
         }
     } else {
         if (0 != (ret = dblayer_instance_close(job->inst->inst_be))) {
