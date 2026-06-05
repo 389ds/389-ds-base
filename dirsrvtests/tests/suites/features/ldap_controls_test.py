2176f83fc738e88a331f5055d1fb2e2c7fccf5a9
Viktor Ashirov <vashirov@redhat.com>
2026-06-05 11:17:35 +0200

    Fix test389 imports on older branches

--- dirsrvtests/tests/suites/features/ldap_controls_test.py
+++ dirsrvtests/tests/suites/features/ldap_controls_test.py
@@ -12,7 +12,7 @@ import ldap
 from ldap.controls import RequestControl
 from ldap.controls.readentry import PostReadControl
 from lib389.idm.user import UserAccounts, UserAccount
-from test389.topologies import topology_st
+from lib389.topologies import topology_st
 from lib389._constants import DEFAULT_SUFFIX, DN_DM, PASSWORD
 
 pytestmark = pytest.mark.tier1
